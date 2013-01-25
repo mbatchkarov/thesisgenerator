1cc0798a94612f3df982cd442a3c4eb114443ef1
Miroslav Batchkarov <M.Batchkarov@sussex.ac.uk>
2013-01-25 15:32:55 +0000

    fixed incorrect package name in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='thesisgenerator',
     version='0.1',
-    packages=['iterpipes'],
+    packages=['thesisgenerator', 'thesisgenerator.plugins', 'thesisgenerator.test'],
     author=['Matti Lyra', 'Miroslav Batchkarov'],
     author_email=['M.Lyra@sussex.ac.uk','M.Batchkarov@sussex.ac.uk']
     )
