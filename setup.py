a97de21c1bf23e031a78e0633578f67c3eed3712
Miroslav Batchkarov <M.Batchkarov@sussex.ac.uk>
2013-01-25 18:34:16 +0000

    updated setup.py to include more (probably not all) of the project's dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 
 setup(
     name='thesisgenerator',
-    version='0.1',
+    version='0.2',
     packages=['thesisgenerator', 'thesisgenerator.plugins', 'thesisgenerator.test'],
     author=['Matti Lyra', 'Miroslav Batchkarov'],
-    author_email=['M.Lyra@sussex.ac.uk','M.Batchkarov@sussex.ac.uk']
+    author_email=['M.Lyra@sussex.ac.uk','M.Batchkarov@sussex.ac.uk'],
+    requires=['iterpipes', 'pandas', 'matplotlib', 'numpy', 'scipy', 'scikit-learn']
     )
 
