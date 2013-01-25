cf426a2a4c2ab9a7e6967a2d9046a1e915b1a7e3
Miroslav Batchkarov <M.Batchkarov@sussex.ac.uk>
2013-01-25 15:05:04 +0000

    moved things around so the application can be easily run from the command line

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='thesisgenerator',
+    version='0.1',
+    packages=['iterpipes'],
+    author=['Matti Lyra', 'Miroslav Batchkarov'],
+    author_email=['M.Lyra@sussex.ac.uk','M.Batchkarov@sussex.ac.uk']
+    )
+
