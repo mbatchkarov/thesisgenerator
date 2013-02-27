7829afd8837d2c1ebe6a8d545c81103dd42bbaf7
Miroslav Batchkarov <M.Batchkarov@sussex.ac.uk>
2013-02-27 16:43:14 +0000

    improved plotting code so that resultant bar chart is more legible and applies to experiments 7-12

--- thesisgenerator/plugins/visualise.py
+++ thesisgenerator/plugins/visualise.py
@@ -1,7 +1,6 @@
 import operator
 
 import numpy
-import matplotlib
 
 
 __author__ = 'mmb28'
@@ -25,46 +24,45 @@ def query_to_data_frame(sql):
         return psql.frame_query(sql, con)
 
 
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        plt.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-            '%d' % int(height),
-            ha='center', va='bottom')
-
-
 # <codecell>
 
-data_frames = []
-for table in range(9, 12):
-    for classifier in ['LinearSVC', 'BernoulliNB']:
-        sql = "SELECT * FROM data%.2d where metric == \"macroavg_f1\" and" \
-              " classifier == \"%s\"" % (table, classifier)
-        print sql
-        data_frames.append(('%.2d-%s' % (table, classifier),
-                            query_to_data_frame(sql)))
+def performance_bar_chart(tables, classifiers,width = 0.13):
+    data_frames = []
+    for table in tables:
+        for classifier in classifiers:
+            sql = "SELECT * FROM data%.2d where metric == \"macroavg_f1\" and" \
+                  " classifier == \"%s\"" % (table, classifier)
+            print sql
+            data_frames.append(('%.2d-%s' % (table, classifier),
+                                query_to_data_frame(sql)))
+           # the width of the bars
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cm = plt.get_cmap('gist_rainbow')
+    num_groups = len(data_frames)
+    # todo either predefine a set of colors of a set of hatch styles, not both
+    ax.set_color_cycle([cm(1. * i / num_groups) for i in range(num_groups)])
+    for (i, (name, df)) in enumerate(data_frames):
+        color = cm(1. * i / num_groups)
+        x = numpy.arange(len(df['sample_size']))
+        y = df['score_mean']
+        yerr = df['score_std']
+        ax.bar(x + i * width, y, width, yerr=yerr, color=color, ecolor='black',
+            linewidth=0, hatch="\\-/x*."[i % len(data_frames)])
+        ax.set_xlabel('Sample size')
+        ax.set_ylabel('Macroavg F1')
+        # ax.set_title(
+        #     'Earn vs not-earn with different thesauri and training data')
+        ax.set_xticks(x + (i / 2) * width + width)
+        ax.set_xticklabels(tuple(df['sample_size']))
+    ax.legend(map(operator.itemgetter(0), data_frames), 'lower right')
+    exp_range = '-'.join(map(str, tables))
+    plt.savefig('figures/exp%s-performa.png'%exp_range, format='png',
+        dpi=300)
+    # plt.show()
 
-width = 0.1       # the width of the bars
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cm = plt.get_cmap('gist_rainbow')
-NUM_COLORS = 10
-ax.set_color_cycle([cm(1. * i / NUM_COLORS) for i in range(NUM_COLORS)])
 
-for (i, (name, df)) in enumerate(data_frames):
-    color = cm(1. * i / NUM_COLORS)
-    x = numpy.arange(len(df['sample_size']))
-    y = df['score_mean']
-    yerr = df['score_std']
-    ax.bar(x + i * width, y, width, yerr=yerr, color=color)
-    ax.set_xlabel('Sample size')
-    ax.set_ylabel('Macroavg F1')
-    ax.set_title('Earn vs not-earn with different thesauri and training data')
-    ax.set_xticks(x + width, tuple(df['sample_size']))
-
-ax.legend(map(operator.itemgetter(0), data_frames), 'lower right')
-plt.savefig('figures/exp9-11-perf.png', format='png', dpi=fig.dpi)
-plt.show()
+performance_bar_chart([7,8], ['LinearSVC'])
+performance_bar_chart(range(9,12), ['LinearSVC', 'BernoulliNB'])
 
 print 'done'
