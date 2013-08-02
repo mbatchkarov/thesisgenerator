0c5172db3e1a454c92ad24d2dff9d92d49e91518
Miroslav Batchkarov <mbatchkarov@gmail.com>
2013-08-02 11:35:22 +0100

    [finishes #54376368] tidied up experimental_utils

--- thesisgenerator/plugins/experimental_utils.py
+++ thesisgenerator/plugins/experimental_utils.py
@@ -5,6 +5,7 @@
 # thesisgenerator package would not be on the path, add it and try again
 import logging
 import sys
+from thesisgenerator.plugins.file_generators import _exp16_file_iterator, _exp1_file_iterator, _vary_training_size_file_iterator
 
 sys.path.append('.')
 sys.path.append('..')
@@ -18,8 +19,7 @@ from thesisgenerator.plugins import thesaurus_loader, tokenizers
 import glob
 from itertools import chain
 import os
-import shutil
-import itertools
+from joblib import Parallel, delayed
 
 from numpy import nonzero
 
@@ -60,29 +60,6 @@ def _init_utilities_state(config):
     return th1, tok
 
 
-def _nested_set(dic, key_list, value):
-    """
-    >>> d = {}
-    >>> nested_set(d, ['person', 'address', 'city'], 'New York')
-    >>> d
-    {'person': {'address': {'city': 'New York'}}}
-    """
-    for key in key_list[:-1]:
-        dic = dic.setdefault(key, {})
-    dic[key_list[-1]] = value
-
-
-def replace_in_file(conf_file, keys, new_value):
-    if type(keys) is str:
-        # handle the case when there is a single key
-        keys = [keys]
-
-    config_obj, configspec_file = parse_config_file(conf_file)
-    _nested_set(config_obj, keys, new_value)
-    with open(conf_file, 'w') as outfile:
-        config_obj.write(outfile)
-
-
 def inspect_thesaurus_effect(outdir, clf_name, thesaurus_file, pipeline,
                              predicted, x_test):
     """
@@ -108,167 +85,6 @@ def inspect_thesaurus_effect(outdir, clf_name, thesaurus_file, pipeline,
         outfile.write('Decisions changed: %d' % (
             nonzero(predicted - predicted2)[0].shape[0]))
         outfile.write('\n')
-
-
-def _write_exp1_conf_file(base_conf_file, run_id, thes):
-    """
-    Create a bunch of copies of the base conf file and in the new one modifies
-    the experiment name and the (SINGLE) thesaurus used
-    """
-    log_file, new_conf_file = _prepare_conf_files(base_conf_file, 1,
-                                                  run_id)
-
-    replace_in_file(new_conf_file, 'name', 'exp%d-%d' % (1, run_id))
-    replace_in_file(new_conf_file, 'debug', False)
-
-    # it is important that the list of thesaurus files in the conf file ends with a comma
-    replace_in_file(new_conf_file, ['feature_extraction', 'thesaurus_files'],
-                    thes)
-    return new_conf_file, log_file
-
-
-def _exp1_file_iterator(pattern, base_conf_file):
-    """
-    Generates a conf file for each thesaurus file matching the provided glob
-    pattern. Each conf file is a single classification run
-    """
-    thesauri = glob.glob(pattern)
-
-    for id, t in enumerate(thesauri):
-        new_conf_file, log_file = _write_exp1_conf_file(base_conf_file, id, t)
-        yield new_conf_file, log_file
-    raise StopIteration
-
-
-def _prepare_conf_files(base_conf_file, exp_id, run_id):
-    """
-    Takes a conf files and moves it to a subdirectory to be modified later.
-    Also creates a log file for the classification run that will be run with
-    the newly created conf file
-     Parameter id identifies the clone and distinguishes it from other clones
-    """
-    name, ext = os.path.splitext(base_conf_file)
-    name = '%s-variants' % name
-    if not os.path.exists(name):
-        os.mkdir(name)
-    new_conf_file = os.path.join(name, 'exp%d-%d%s' % (exp_id, run_id, ext))
-    log_file = os.path.join(name, '..', 'logs')
-    shutil.copy(base_conf_file, new_conf_file)
-    return log_file, new_conf_file
-
-
-def _write_exp2_to_14_conf_file(base_conf_file, exp_id, run_id,
-                                sample_size):
-    """
-    Prepares conf files for exp2-14 by altering the sample size parameter in
-    the
-     provided base conf file
-    """
-    log_dir, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
-                                                 run_id)
-
-    replace_in_file(new_conf_file, 'name', 'exp%d-%d' % (exp_id, run_id))
-    replace_in_file(new_conf_file, ['crossvalidation', 'sample_size'],
-                    sample_size)
-    # replace_in_file(new_conf_file, 'debug', False)
-    return new_conf_file, log_dir
-
-
-# def _write_exp22conf_file(base_conf_file, exp_id, run_id, sample_size,
-#                           keep_only_IT, use_signifier_only):
-#     """
-#     Prepares conf files for exp22 by altering the sample_size,
-#     use_signifier_only and keep_only_IT  parameters in the provided base
-#     conf file
-#     """
-#     log_dir, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
-#                                                  run_id)
-#
-#     replace_in_file(new_conf_file, 'name', 'exp%d-%d' % (exp_id, run_id))
-#     replace_in_file(new_conf_file, ['crossvalidation', 'sample_size'],
-#                     sample_size)
-#     replace_in_file(new_conf_file, ['feature_extraction', 'use_signifier_only'],
-#                     use_signifier_only)
-#     replace_in_file(new_conf_file, ['tokenizer', 'keep_only_IT'], keep_only_IT)
-#     replace_in_file(new_conf_file, 'debug', False)
-#     return new_conf_file, log_dir
-
-
-def _vary_training_size_file_iterator(sizes, exp_id, conf_file):
-    for sub_id, size in enumerate(sizes):
-        new_conf_file, log_file = _write_exp2_to_14_conf_file(conf_file,
-                                                              exp_id,
-                                                              sub_id,
-                                                              size)
-        print 'Yielding %s, %s' % (new_conf_file, new_conf_file)
-        yield new_conf_file, log_file
-    raise StopIteration
-
-
-# def _exp22_file_iterator(sizes, exp_id, conf_file):
-#     sub_id = 0
-#     for size in sizes:
-#         for keep_only_IT in [True, False]:
-#             for use_signifier_only in [True, False]:
-#                 new_conf_file, log_file = _write_exp22conf_file(conf_file,
-#                                                                 exp_id,
-#                                                                 sub_id, size,
-#                                                                 keep_only_IT,
-#                                                                 use_signifier_only)
-#                 print 'Yielding %s, %s' % (new_conf_file, new_conf_file)
-#                 yield new_conf_file, log_file
-#                 sub_id += 1
-#     raise StopIteration
-
-
-def _write_exp15_conf_file(base_conf_file, exp_id, run_id, shuffle):
-    log_file, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
-                                                  run_id)
-    replace_in_file(new_conf_file, 'name',
-                    'exp%d-%d' % (exp_id, run_id))
-    replace_in_file(new_conf_file, 'shuffle_targets',
-                    'shuffle_targets' % shuffle)
-    replace_in_file(new_conf_file, 'debug', False)
-    return new_conf_file, log_file
-
-
-def _exp15_file_iterator(conf_file):
-    for id, shuffle in enumerate([True, False]):
-        new_conf_file, log_file = _write_exp15_conf_file(conf_file, 15,
-                                                         id, shuffle)
-        print 'Yielding %s, %s' % (new_conf_file, log_file)
-        yield new_conf_file, log_file
-    raise StopIteration
-
-
-def _write_exp16_conf_file(base_conf_file, exp_id, run_id,
-                           thesauri_list,
-                           normalize):
-    log_file, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
-                                                  run_id)
-    replace_in_file(new_conf_file, 'name',
-                    'exp%d-%d' % (exp_id, run_id))
-    replace_in_file(new_conf_file, ['feature_extraction', 'thesaurus_files'],
-                    thesauri_list)
-    replace_in_file(new_conf_file, ['feature_extraction', 'normalise_entities'],
-                    normalize)
-    replace_in_file(new_conf_file, 'debug', False)
-    return new_conf_file, log_file
-
-
-def _exp16_file_iterator(conf_file):
-    thesauri = [
-        "/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12a/exp6.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12b/exp6.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12c/exp6.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12d/exp6.sims.neighbours.strings,",
-        "/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12a/exp7.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12b/exp7.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12c/exp7.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12d/exp7.sims.neighbours.strings,"
-    ]
-    for id, (thesauri, normalize) in enumerate(
-            itertools.product(thesauri, [True, False])):
-        new_conf_file, log_file = _write_exp16_conf_file(conf_file,
-                                                         16, id,
-                                                         thesauri, normalize)
-        print 'Yielding %s, %s' % (new_conf_file, log_file)
-        yield new_conf_file, log_file
-    raise StopIteration
 
 
 def load_text_data_into_memory(options):
@@ -372,41 +188,26 @@ def _get_data_iterators(**kwargs):
     return data_iterable, targets_iterable
 
 
-def evaluate_thesauri(base_conf_file, file_iterator,
-                      reload_data=False, pool_size=1):
+def tokenize_data(base_conf_file):
     config_obj, configspec_file = parse_config_file(base_conf_file)
-    from joblib import Parallel, delayed
-
-    if reload_data:
-        data = None
-        print "WARNING: raw dataset will be reloaded before each " \
-              "sub-experiment"
-    else:
-        # read the raw text just once
-        options = {'input': config_obj['feature_extraction']['input'],
-                   'shuffle_targets': config_obj['shuffle_targets']}
-
-        try:
-            options['input_generator'] = config_obj['feature_extraction'][
-                'input_generator']
-        except KeyError:
-            options['input_generator'] = ''
-        options['source'] = config_obj['training_data']
-        if config_obj['test_data']:
-            options['test_data'] = config_obj['test_data']
-
-        print 'Loading training data...'
-
-        x_tr, y_tr, x_test, y_test = load_text_data_into_memory(options)
-
-    th1, tok = _init_utilities_state(config_obj)
-    x_tr = map(tok.tokenize, x_tr)
-    x_test = map(tok.tokenize, x_test)
+    # read the raw text just once
+    options = {'input': config_obj['feature_extraction']['input'],
+               'shuffle_targets': config_obj['shuffle_targets']}
+    try:
+        options['input_generator'] = config_obj['feature_extraction'][
+            'input_generator']
+    except KeyError:
+        options['input_generator'] = ''
+    options['source'] = config_obj['training_data']
+    if config_obj['test_data']:
+        options['test_data'] = config_obj['test_data']
+    print 'Loading training data...'
+    x_tr, y_tr, x_test, y_test = load_text_data_into_memory(options)
+    thesaurus, tokenizer = _init_utilities_state(config_obj)
+    x_tr = map(tokenizer.tokenize, x_tr)
+    x_test = map(tokenizer.tokenize, x_test)
     data = (x_tr, y_tr, x_test, y_test)
-
-    Parallel(n_jobs=pool_size)(delayed(go)(new_conf_file, log_file,
-                                           data, th1) for
-                               new_conf_file, log_file in file_iterator)
+    return data, thesaurus
 
 
 def _clear_old_files(i, prefix):
@@ -448,27 +249,25 @@ def run_experiment(i, num_workers=4,
     if predefined_sized:
         sizes = predefined_sized
 
-    reload_data = False
     # ----------- EXPERIMENT 1 -----------
     base_conf_file = '%s/conf/exp%d/exp%d_base.conf' % (prefix, i, i)
     if i == 1:
-        it = _exp1_file_iterator(exp1_thes_pattern,
-                                 '%s/conf/exp1/exp1_base.conf' % prefix)
+        conf_file_iterator = _exp1_file_iterator(exp1_thes_pattern,
+                                                 '%s/conf/exp1/exp1_base.conf' % prefix)
 
     # ----------- EXPERIMENTS 2-14 -----------
     elif i == 0 or 1 < i <= 14 or 17 <= i <= 28:
-        it = _vary_training_size_file_iterator(sizes, i, base_conf_file)
-    elif i == 15:
-        it = _exp15_file_iterator(base_conf_file)
-        reload_data = True
+        conf_file_iterator = _vary_training_size_file_iterator(sizes, i, base_conf_file)
     elif i == 16:
-        it = _exp16_file_iterator(base_conf_file)
+        conf_file_iterator = _exp16_file_iterator(base_conf_file)
     else:
         raise ValueError('No such experiment number: %d' % i)
 
     _clear_old_files(i, prefix)
-    evaluate_thesauri(base_conf_file, it, pool_size=num_workers,
-                      reload_data=reload_data)
+    data, thesurus = tokenize_data(base_conf_file)
+    # run the data through the pipeline
+    Parallel(n_jobs=num_workers)(delayed(go)(new_conf_file, log_file, data, thesurus) for
+                                 new_conf_file, log_file in conf_file_iterator)
 
     # ----------- CONSOLIDATION -----------
     output_dir = '%s/conf/exp%d/output/' % (prefix, i)
---
+++ thesisgenerator/plugins/file_generators.py
@@ -0,0 +1,155 @@
+import glob
+import itertools
+import os
+import shutil
+from thesisgenerator.__main__ import parse_config_file
+
+__author__ = 'mmb28'
+
+
+def _exp15_file_iterator(conf_file):
+    for id, shuffle in enumerate([True, False]):
+        new_conf_file, log_file = _write_exp15_conf_file(conf_file, 15,
+                                                         id, shuffle)
+        print 'Yielding %s, %s' % (new_conf_file, log_file)
+        yield new_conf_file, log_file
+    raise StopIteration
+
+
+def _exp16_file_iterator(conf_file):
+    thesauri = [
+        "/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12a/exp6.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12b/exp6.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12c/exp6.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp6-12d/exp6.sims.neighbours.strings,",
+        "/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12a/exp7.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12b/exp7.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12c/exp7.sims.neighbours.strings,/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp7-12d/exp7.sims.neighbours.strings,"
+    ]
+    for id, (thesauri, normalize) in enumerate(
+            itertools.product(thesauri, [True, False])):
+        new_conf_file, log_file = _write_exp16_conf_file(conf_file,
+                                                         16, id,
+                                                         thesauri, normalize)
+        print 'Yielding %s, %s' % (new_conf_file, log_file)
+        yield new_conf_file, log_file
+    raise StopIteration
+
+
+def _exp1_file_iterator(pattern, base_conf_file):
+    """
+    Generates a conf file for each thesaurus file matching the provided glob
+    pattern. Each conf file is a single classification run
+    """
+    thesauri = glob.glob(pattern)
+
+    for id, t in enumerate(thesauri):
+        new_conf_file, log_file = _write_exp1_conf_file(base_conf_file, id, t)
+        yield new_conf_file, log_file
+    raise StopIteration
+
+
+def _vary_training_size_file_iterator(sizes, exp_id, conf_file):
+    for sub_id, size in enumerate(sizes):
+        new_conf_file, log_file = _write_exp2_to_14_conf_file(conf_file,
+                                                              exp_id,
+                                                              sub_id,
+                                                              size)
+        print 'Yielding %s, %s' % (new_conf_file, new_conf_file)
+        yield new_conf_file, log_file
+    raise StopIteration
+
+
+def _write_exp15_conf_file(base_conf_file, exp_id, run_id, shuffle):
+    log_file, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
+                                                  run_id)
+    replace_in_file(new_conf_file, 'name',
+                    'exp%d-%d' % (exp_id, run_id))
+    replace_in_file(new_conf_file, 'shuffle_targets',
+                    'shuffle_targets' % shuffle)
+    replace_in_file(new_conf_file, 'debug', False)
+    return new_conf_file, log_file
+
+
+def _write_exp16_conf_file(base_conf_file, exp_id, run_id,
+                           thesauri_list,
+                           normalize):
+    log_file, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
+                                                  run_id)
+    replace_in_file(new_conf_file, 'name',
+                    'exp%d-%d' % (exp_id, run_id))
+    replace_in_file(new_conf_file, ['feature_extraction', 'thesaurus_files'],
+                    thesauri_list)
+    replace_in_file(new_conf_file, ['feature_extraction', 'normalise_entities'],
+                    normalize)
+    replace_in_file(new_conf_file, 'debug', False)
+    return new_conf_file, log_file
+
+
+def _write_exp1_conf_file(base_conf_file, run_id, thes):
+    """
+    Create a bunch of copies of the base conf file and in the new one modifies
+    the experiment name and the (SINGLE) thesaurus used
+    """
+    log_file, new_conf_file = _prepare_conf_files(base_conf_file, 1,
+                                                  run_id)
+
+    replace_in_file(new_conf_file, 'name', 'exp%d-%d' % (1, run_id))
+    replace_in_file(new_conf_file, 'debug', False)
+
+    # it is important that the list of thesaurus files in the conf file ends with a comma
+    replace_in_file(new_conf_file, ['feature_extraction', 'thesaurus_files'],
+                    thes)
+    return new_conf_file, log_file
+
+
+def _write_exp2_to_14_conf_file(base_conf_file, exp_id, run_id,
+                                sample_size):
+    """
+    Prepares conf files for exp2-14 by altering the sample size parameter in
+    the
+     provided base conf file
+    """
+    log_dir, new_conf_file = _prepare_conf_files(base_conf_file, exp_id,
+                                                 run_id)
+
+    replace_in_file(new_conf_file, 'name', 'exp%d-%d' % (exp_id, run_id))
+    replace_in_file(new_conf_file, ['crossvalidation', 'sample_size'],
+                    sample_size)
+    # replace_in_file(new_conf_file, 'debug', False)
+    return new_conf_file, log_dir
+
+
+def replace_in_file(conf_file, keys, new_value):
+    if type(keys) is str:
+        # handle the case when there is a single key
+        keys = [keys]
+
+    config_obj, configspec_file = parse_config_file(conf_file)
+    _nested_set(config_obj, keys, new_value)
+    with open(conf_file, 'w') as outfile:
+        config_obj.write(outfile)
+
+
+def _nested_set(dic, key_list, value):
+    """
+    >>> d = {}
+    >>> nested_set(d, ['person', 'address', 'city'], 'New York')
+    >>> d
+    {'person': {'address': {'city': 'New York'}}}
+    """
+    for key in key_list[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[key_list[-1]] = value
+
+
+def _prepare_conf_files(base_conf_file, exp_id, run_id):
+    """
+    Takes a conf files and moves it to a subdirectory to be modified later.
+    Also creates a log file for the classification run that will be run with
+    the newly created conf file
+     Parameter id identifies the clone and distinguishes it from other clones
+    """
+    name, ext = os.path.splitext(base_conf_file)
+    name = '%s-variants' % name
+    if not os.path.exists(name):
+        os.mkdir(name)
+    new_conf_file = os.path.join(name, 'exp%d-%d%s' % (exp_id, run_id, ext))
+    log_file = os.path.join(name, '..', 'logs')
+    shutil.copy(base_conf_file, new_conf_file)
+    return log_file, new_conf_file
