c0ba47c056c9436008962b5209800cd31d621d44
Miroslav Batchkarov <mbatchkarov@gmail.com>
2013-10-15 13:31:14 +0100

    cleaned up a unit test a little bit

--- thesisgenerator/tests/test_feature_selectors.py
+++ thesisgenerator/tests/test_feature_selectors.py
@@ -22,7 +22,7 @@ class TestVectorBackedSelectKBest(TestCase):
         """{ ('1-GRAM', ('X',)) : int} -> {'X' : int}"""
         for k, v in mydict.iteritems():
             self.assertEquals(len(k), 2)
-        return {k[1][0]: v for k, v in mydict.iteritems()}
+        return {' '.join(k[1]): v for k, v in mydict.iteritems()}
 
     def _do_feature_selection(self, ensure_vectors_exist, k, use_composer=False):
         """
@@ -55,18 +55,12 @@ class TestVectorBackedSelectKBest(TestCase):
         composers = CompositeVectorSource(unigrams_vectors, composer_list, 0.0, False)
 
         pipeline_list = [
-            ('vect', ThesaurusVectorizer(exp_name='unittests')),
+            ('vect', ThesaurusVectorizer(min_df=1, vector_source=composers, use_tfidf=False)),
             ('fs', VectorBackedSelectKBest(vector_source=composers, ensure_vectors_exist=ensure_vectors_exist, k=k)),
             ('dumper', FeatureVectorsCsvDumper('fs-test'))
         ]
         self.p = Pipeline(pipeline_list)
 
-        call_args = {
-            'vect__vector_source': composers,
-            'vect__use_tfidf': False,
-            'vect__min_df': 1,
-        }
-        self.p.set_params(**call_args)
         tr_matrix, tr_voc = self.p.fit_transform(x_train, y_train)
         ev_matrix, ev_voc = self.p.transform(x_test)
 
@@ -104,110 +98,114 @@ class TestVectorBackedSelectKBest(TestCase):
                  [0., 1., 0., 0., 0., 0.]]))
             self._check_debug_file(ev_matrix, tr_matrix, voc)
 
-    def test_with_thesaurus_feature_selection_only(self):
-        """
-        Tests if features in the training data not contained in the vector source are removed
-        """
-        tr_matrix, tr_voc, ev_matrix, ev_voc = self._do_feature_selection(True, 'all')
 
-        voc = {
-            'cat/n': 0,
-            'dog/n': 1,
-            'game/n': 2,
-            #'kid/n': 3, # removed because vector is missing, this happens in self._do_feature_selection
-            'like/v': 3,
-            'play/v': 4
-        }
-        self.assertDictEqual(tr_voc, voc)
+def test_with_thesaurus_feature_selection_only(self):
+    """
+    Tests if features in the training data not contained in the vector source are removed
+    """
+    tr_matrix, tr_voc, ev_matrix, ev_voc = self._do_feature_selection(True, 'all')
 
-        self.assertDictEqual(tr_voc, ev_voc)
+    voc = {
+        'cat/n': 0,
+        'dog/n': 1,
+        'game/n': 2,
+        #'kid/n': 3, # removed because vector is missing, this happens in self._do_feature_selection
+        'like/v': 3,
+        'play/v': 4
+    }
+    self.assertDictEqual(tr_voc, voc)
 
-        t.assert_array_equal(tr_matrix,
-                             np.array(
-                                 [[1., 1., 0., 1., 0.],
-                                  [1., 1., 0., 1., 0.],
-                                  [0., 0., 1., 0., 1.]]))
-        t.assert_array_equal(ev_matrix,
-                             np.array(
-                                 [[0., 0., 0., 1., 0.],
-                                  [0., 0., 0., 1., 0.],
-                                  [0., 1., 0., 0., 0.]]))
-        self._check_debug_file(ev_matrix, tr_matrix, voc)
+    self.assertDictEqual(tr_voc, ev_voc)
 
-    def test_with_chi2_feature_selection_only(self):
-        """
-        Test the normal case of feature selection, where some number of features are removed because they are
-        not informative
-        """
-        tr_matrix, tr_voc, ev_matrix, ev_voc = self._do_feature_selection(False, 3)
+    t.assert_array_equal(tr_matrix,
+                         np.array(
+                             [[1., 1., 0., 1., 0.],
+                              [1., 1., 0., 1., 0.],
+                              [0., 0., 1., 0., 1.]]))
+    t.assert_array_equal(ev_matrix,
+                         np.array(
+                             [[0., 0., 0., 1., 0.],
+                              [0., 0., 0., 1., 0.],
+                              [0., 1., 0., 0., 0.]]))
+    self._check_debug_file(ev_matrix, tr_matrix, voc)
 
-        # feature scores at train time are [ 1.  1.  2.  2.  1.  2.]. These are provided by sklearn and I have not
-        # verified them
-        voc = {
-            #'cat/n': 0, # removed because their chi2 score is low
-            #'dog/n': 1,
-            'game/n': 0,
-            'kid/n': 1,
-            #'like/v': 4,
-            'play/v': 2
-        }
-        self.assertDictEqual(tr_voc,
-                             voc)
 
-        self.assertDictEqual(tr_voc, ev_voc)
+def test_with_chi2_feature_selection_only(self):
+    """
+    Test the normal case of feature selection, where some number of features are removed because they are
+    not informative
+    """
+    tr_matrix, tr_voc, ev_matrix, ev_voc = self._do_feature_selection(False, 3)
 
-        t.assert_array_equal(tr_matrix,
-                             np.array(
-                                 [[0., 0., 0.],
-                                  [0., 0., 0.],
-                                  [1., 1., 1.]]))
+    # feature scores at train time are [ 1.  1.  2.  2.  1.  2.]. These are provided by sklearn and I have not
+    # verified them. Higher seems to be better (the textbook implementation of chi2 says lower is better)
+    voc = {
+        #'cat/n': 0, # removed because their chi2 score is low
+        #'dog/n': 1,
+        'game/n': 0,
+        'kid/n': 1,
+        #'like/v': 4,
+        'play/v': 2
+    }
+    self.assertDictEqual(tr_voc,
+                         voc)
 
-        t.assert_array_equal(ev_matrix,
-                             np.array(
-                                 [[0., 0., 0.],
-                                  [0., 0., 0.],
-                                  [0., 0., 0.]]))
-        self._check_debug_file(ev_matrix, tr_matrix, voc)
+    self.assertDictEqual(tr_voc, ev_voc)
 
-    def test_with_chi2_and_thesaurus_feature_selection(self):
-        """
-        Test a combination of feature selection through vector source and low chi2 score.
-        """
-        tr_matrix, tr_voc, ev_matrix, ev_voc = self._do_feature_selection(True, 2)
+    t.assert_array_equal(tr_matrix,
+                         np.array(
+                             [[0., 0., 0.],
+                              [0., 0., 0.],
+                              [1., 1., 1.]]))
 
-        self.assertDictEqual(tr_voc, ev_voc)
-        voc = {
-            #'cat/n': 0, # removed because of low chi2 score
-            #'dog/n': 1,  # removed because of low chi2 score
-            'game/n': 0,
-            #'kid/n': 3, # removed because vector is missing
-            #'like/v': 4,  # removed because of low chi2 score
-            'play/v': 1
-        }
-        # feature scores at train time are [ 1.  1.  2.  2.  1.  2.]
-        self.assertDictEqual(tr_voc, voc)
+    t.assert_array_equal(ev_matrix,
+                         np.array(
+                             [[0., 0., 0.],
+                              [0., 0., 0.],
+                              [0., 0., 0.]]))
+    self._check_debug_file(ev_matrix, tr_matrix, voc)
 
-        t.assert_array_equal(tr_matrix, np.array(
-            [[0., 0.],
-             [0., 0.],
-             [1., 1.]]))
-        t.assert_array_equal(ev_matrix, np.array(
-            [[0., 0.],
-             [0., 0.],
-             [0., 0.]]))
 
-        self._check_debug_file(ev_matrix, tr_matrix, voc)
+def test_with_chi2_and_thesaurus_feature_selection(self):
+    """
+    Test a combination of feature selection through vector source and low chi2 score.
+    """
+    tr_matrix, tr_voc, ev_matrix, ev_voc = self._do_feature_selection(True, 2)
 
-    def _check_debug_file(self, ev_matrix, tr_matrix, voc):
-        for name, matrix in zip(['tr', 'ev'], [tr_matrix, ev_matrix]):
-            filename = "PostVectDump_fs-test_%s-cl0-fold'NONE'.csv" % name
-            df = read_csv(filename)
-            # the columns are u'id', u'target', u'total_feat_weight', u'nonzero_feats', followed by feature vectors
-            # check that we have the right number of columns
-            self.assertEquals(len(df.columns), 4 + len(voc))
-            # check that column names match the vocabulary (after stripping feature metadata)
-            self.assertDictEqual(voc, self._strip({eval(v): i for i, v in enumerate(df.columns[4:])}))
-            #check that feature vectors are written correctly
-            t.assert_array_equal(matrix, df.ix[:, 4:].as_matrix())
-            os.remove(filename)
+    self.assertDictEqual(tr_voc, ev_voc)
+    voc = {
+        #'cat/n': 0, # removed because of low chi2 score
+        #'dog/n': 1,  # removed because of low chi2 score
+        'game/n': 0,
+        #'kid/n': 3, # removed because vector is missing
+        #'like/v': 4,  # removed because of low chi2 score
+        'play/v': 1
+    }
+    # feature scores at train time are [ 1.  1.  2.  2.  1.  2.]
+    self.assertDictEqual(tr_voc, voc)
+
+    t.assert_array_equal(tr_matrix, np.array(
+        [[0., 0.],
+         [0., 0.],
+         [1., 1.]]))
+    t.assert_array_equal(ev_matrix, np.array(
+        [[0., 0.],
+         [0., 0.],
+         [0., 0.]]))
+
+    self._check_debug_file(ev_matrix, tr_matrix, voc)
+
+
+def _check_debug_file(self, ev_matrix, tr_matrix, voc):
+    for name, matrix in zip(['tr', 'ev'], [tr_matrix, ev_matrix]):
+        filename = "PostVectDump_fs-test_%s-cl0-fold'NONE'.csv" % name
+        df = read_csv(filename)
+        # the columns are u'id', u'target', u'total_feat_weight', u'nonzero_feats', followed by feature vectors
+        # check that we have the right number of columns
+        self.assertEquals(len(df.columns), 4 + len(voc))
+        # check that column names match the vocabulary (after stripping feature metadata)
+        self.assertDictEqual(voc, self._strip({eval(v): i for i, v in enumerate(df.columns[4:])}))
+        #check that feature vectors are written correctly
+        t.assert_array_equal(matrix, df.ix[:, 4:].as_matrix())
+        os.remove(filename)
 
