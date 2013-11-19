57dad17b6a86adb1974f9ce4da90e420337453a9
Miroslav Batchkarov <mbatchkarov@gmail.com>
2013-11-19 17:58:31 +0000

    Added facility to dimensionality reducer script to filter out infrequent entries to reduce matrix size to make dim. red. computationally feasible. This follows Baroni's idea that vectors should only be collected for the most common words

--- thesisgenerator/scripts/reduce_dimensionality.py
+++ thesisgenerator/scripts/reduce_dimensionality.py
@@ -11,26 +11,65 @@ import numpy, scipy, time
 from thesisgenerator.scripts import dump_all_composed_vectors as dump
 from thesisgenerator.plugins.tokenizers import DocumentFeature
 from thesisgenerator.composers.utils import write_vectors_to_disk
+import numpy as np
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s\t%(module)s.%(funcName)s ""(line %(lineno)d)\t%(levelname)s : %(""message)s")
 
 
-def do_work(paths):
-    thesaurus = Thesaurus(paths)
-
-    n_components = 10
+def do_work(vector_file_paths,
+            word_limits=[('N', 8000), ('V', 4000), ('J', 4000), ('RB', 2000)],
+            reduce_to=[300, 1000, 5000]):
+    thesaurus = Thesaurus(vector_file_paths)
     mat, cols, rows = thesaurus.to_sparse_matrix()
-    features = [DocumentFeature.from_string(x) for x in rows]
-    feature_types = [x.type for x in features]
+    logging.info('Loaded a data matrix of shape %r', mat.shape)
+
+    features = [DocumentFeature.from_string(r) for r in rows] # convert to document feature for access to PoS tag
+
     # don't want to do dimensionality reduction on composed vectors
+    feature_types = [sorted_idx_and_pos_matching.type for sorted_idx_and_pos_matching in features]
     assert all(x == '1-GRAM' for x in feature_types)
-    feature_pos = [x.tokens[0].pos for x in features]
+
+    # get the PoS tags of each row in the matrix
+    pos_tags = np.array([df.tokens[0].pos for df in features])
     print mat.sum()
 
-    for n_components in [300, 1000, 5000]:
+    # find the rows of the matrix that correspond to the most frequent nouns, verbs, ...,
+    # as measured by sum of feature counts. This is Byblo's definition of frequency (which is in fact a marginal),
+    # but it is strongly correlated with one normally thinks of as entry frequency
+    pos_to_rows = {}
+    for desired_pos, desired_count in word_limits:
+        row_of_current_pos = pos_tags == desired_pos # what rows are the right PoS tags at, boolean mask array
+        # indices of the array sorted by row sum, and where the pos == desired_pos
+        sorted_idx_by_sum = np.ravel(mat.sum(1)).argsort()
+        row_of_current_pos = row_of_current_pos[sorted_idx_by_sum]
+        sorted_idx_and_pos_matching = sorted_idx_by_sum[row_of_current_pos]
+        # slice off the top desired_count and store them
+        pos_to_rows[desired_pos] = sorted_idx_and_pos_matching[-desired_count:]
+    desired_rows = np.sort(np.hstack(x for x in pos_to_rows.values()))
+
+    # check that the pos tag of each selected entry is what we think it is
+    for k, v in pos_to_rows.iteritems():
+        assert all(k == x for x in pos_tags[v])
+
+    # remove the vectors for infrequent entries, update list of pos tags too
+    mat = mat[desired_rows, :]
+    rows = np.array(rows)[desired_rows]
+    pos_tags = pos_tags[desired_rows]
+
+    # removing rows may empty some columns, remove these as well
+    desired_cols = np.ravel(mat.sum(0)) > 0
+    mat = mat[:, desired_cols]
+    cols = np.array(cols)[desired_cols]
+    logging.info('Selected only the most frequent entries, matrix size is now %r', mat.shape)
+
+    for n_components in reduce_to:
+        if n_components > mat.shape[1]:
+            logging.error('Cannot reduce dimensionality from %d to %d', mat.shape[1], n_components)
+            continue
+
         method = NMF(n_components)
-        logging.info(mat.shape)
+        logging.info('Reducing dimensionality of matrix of shape %r', mat.shape)
         start = time.time()
         reduced_mat = method.fit_transform(mat)
         end = time.time()
@@ -39,19 +78,17 @@ def do_work(paths):
                                                                                        reduced_mat.shape,
                                                                                        end - start))
 
-        print reduced_mat.sum()
         for path, desired_pos in zip(dump.giga_paths, ['N', 'V', 'J', 'RB']):
             basename = path.split('.')[0]
             basename += '-nmf{}'.format(n_components)
             features_file = basename + '.features.filtered.strings'
             events_file = basename + '.events.filtered.strings'
             entries_file = basename + '.entries.filtered.strings'
-            out_path = '.'.join(basename)
 
-            logging.info('Writing reduces vector files for PoS {} to {}'.format(desired_pos, out_path))
-            row_nums_to_include = [i for i, pos in enumerate(feature_pos) if pos == desired_pos]
-            tmp_mat = scipy.sparse.coo_matrix(reduced_mat[row_nums_to_include, :])
-            write_vectors_to_disk(tmp_mat, numpy.array(features)[row_nums_to_include],
+            logging.info('Writing reduced vector files for PoS {}'.format(desired_pos))
+            rows_for_this_pos = pos_tags == desired_pos
+            tmp_mat = scipy.sparse.coo_matrix(reduced_mat[rows_for_this_pos, :])
+            write_vectors_to_disk(tmp_mat, numpy.array(features)[rows_for_this_pos],
                                   ['NMF:feat{0:05d}'.format(i) for i in range(n_components)],
                                   features_file, entries_file, events_file)
             #'tmp/{}-svd{}.features.txt'.format(desired_pos, n_components),
