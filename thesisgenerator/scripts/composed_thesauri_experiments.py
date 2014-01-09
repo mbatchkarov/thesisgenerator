3efa32793a0166f587e7cde16e9140df351e4fd3
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-01-09 17:11:36 +0000

    cosmetic fix

--- thesisgenerator/scripts/composed_thesauri_experiments.py
+++ thesisgenerator/scripts/composed_thesauri_experiments.py
@@ -9,6 +9,7 @@ from thesisgenerator.utils.cmd_utils import run_and_log_output
 from thesisgenerator.composers.vectorstore import *
 from thesisgenerator.plugins.experimental_utils import run_experiment
 from thesisgenerator.utils.conf_file_utils import set_in_conf_file, parse_config_file
+
 '''
 Once all thesauri with ngram entries (obtained using different composition methods) have been built offline,
 use this script to run them through the classification framework
@@ -19,11 +20,12 @@ __author__ = 'mmb28'
 superbase_conf_file = 'conf/exp42-superbase.conf'
 composer_algos = [AdditiveComposer, MultiplicativeComposer, HeadWordComposer,
                   TailWordComposer, MinComposer, MaxComposer]
+pattern = '/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp10-12AN_NN_gigaw_{}' \
+          '/exp10.sims.neighbours.strings'
 thesauri = [
-    '/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp10-12AN_NN_gigaw_{}' \
-    '/exp10.sims.neighbours.strings'.format(c.name) for c in composer_algos
+    pattern.format(c.name) for c in composer_algos
 ]
-thesauri.append('Observed') # todo path to thesaurus with observed AN/NN vectors added in
+thesauri.append(pattern.format('Observed'))
 
 handler = 'thesisgenerator.plugins.bov_feature_handlers.SignifiedFeatureHandler'
 
@@ -49,4 +51,4 @@ for offset, thes in enumerate(thesauri):
     print config_obj['vector_sources']['unigram_paths']
 
     #run_experiment(first_exp + offset)
-    run_and_log_output('qsub -N composed{0} go-single-experiment.sh {0}'.format(first_exp + offset))
+    # run_and_log_output('qsub -N composed{0} go-single-experiment.sh {0}'.format(first_exp + offset))
