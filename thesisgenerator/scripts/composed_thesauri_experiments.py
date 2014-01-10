562629a138fda2778db24710d15811193da7f5ad
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-01-10 16:43:18 +0000

    Added more conf files for EACL-14 experiment (two thesaurus sources, two labelled data sets)

--- thesisgenerator/scripts/composed_thesauri_experiments.py
+++ thesisgenerator/scripts/composed_thesauri_experiments.py
@@ -20,35 +20,40 @@ __author__ = 'mmb28'
 superbase_conf_file = 'conf/exp42-superbase.conf'
 composer_algos = [AdditiveComposer, MultiplicativeComposer, HeadWordComposer,
                   TailWordComposer, MinComposer, MaxComposer]
+
+# get the names of all GIGAW thesauri
 pattern = '/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp10-12AN_NN_gigaw_{}' \
           '/exp10.sims.neighbours.strings'
-thesauri = [
-    pattern.format(c.name) for c in composer_algos
-]
-thesauri.append(pattern.format('Observed'))
+gigaw_thesauri = [pattern.format(c.name) for c in composer_algos]
+gigaw_thesauri.append(pattern.format('Observed'))
+
+# get the names of all WIKI thesauri
+pattern = '/mnt/lustre/scratch/inf/mmb28/FeatureExtrationToolkit/exp11-12AN_NN_gigaw_{}' \
+          '/exp11.sims.neighbours.strings'
+wiki_thesauri = [pattern.format(c.name) for c in composer_algos]
+wiki_thesauri.append(pattern.format('Observed'))
 
 handler = 'thesisgenerator.plugins.bov_feature_handlers.SignifiedFeatureHandler'
 
+thesauri = {42: gigaw_thesauri, 56: gigaw_thesauri, 49: wiki_thesauri, 63: wiki_thesauri}
 
-# for each Thesaurus
-first_exp = 42
-for offset, thes in enumerate(thesauri):
-    experiment_dir = 'conf/exp%d' % (first_exp + offset)
-    if not os.path.exists(experiment_dir):
-        os.mkdir(experiment_dir)
-    else:
-        # todo do not remove this dir, may contain useful results
-        shutil.rmtree(experiment_dir)
-        os.mkdir(experiment_dir)
-    base_conf_file = os.path.join(experiment_dir, 'exp%d_base.conf' % (first_exp + offset))
-    shutil.copy(superbase_conf_file, base_conf_file)
+for first_exp in [42, 49, 56, 63]:
+    for offset, thes in enumerate(thesauri[first_exp]):
+        experiment_dir = 'conf/exp%d' % (first_exp + offset)
+        if not os.path.exists(experiment_dir):
+            os.mkdir(experiment_dir)
+        else:
+            # todo do not remove this dir, may contain useful results
+            shutil.rmtree(experiment_dir)
+            os.mkdir(experiment_dir)
+        base_conf_file = os.path.join(experiment_dir, 'exp%d_base.conf' % (first_exp + offset))
+        print base_conf_file, thes
+        shutil.copy(superbase_conf_file, base_conf_file)
 
-    set_in_conf_file(base_conf_file, ['vector_sources', 'unigram_paths'], [thes])
-    set_in_conf_file(base_conf_file, ['output_dir'], './conf/exp%d/output' % (first_exp + offset))
-    set_in_conf_file(base_conf_file, ['name'], 'exp%d' % (first_exp + offset))
-    config_obj, configspec_file = parse_config_file(base_conf_file)
-    print base_conf_file
-    print config_obj['vector_sources']['unigram_paths']
+        set_in_conf_file(base_conf_file, ['vector_sources', 'unigram_paths'], [thes])
+        set_in_conf_file(base_conf_file, ['output_dir'], './conf/exp%d/output' % (first_exp + offset))
+        set_in_conf_file(base_conf_file, ['name'], 'exp%d' % (first_exp + offset))
+        config_obj, configspec_file = parse_config_file(base_conf_file)
 
-    #run_experiment(first_exp + offset)
-    # run_and_log_output('qsub -N composed{0} go-single-experiment.sh {0}'.format(first_exp + offset))
+        #run_experiment(first_exp + offset)
+        run_and_log_output('qsub -N composed{0} go-single-experiment.sh {0}'.format(first_exp + offset))
