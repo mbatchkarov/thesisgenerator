mbatchkarov/thesisgenerator

commit bd48d0a973eb5fa6675212ae2bc210dce6bcc49a

Changed it so that at encode time both NPs and unigrams  (N and J) are used as feature, and at decode time only NPs are considered and mapped to known features

conf/exp1-superbase.conf
@@ -11,7 +11,8 @@ joblib_caching = True
     decode_error = replace
     stop_words = english
     min_df = 1
-    ngram_range = 0,0
+    ngram_range = 1,1
+    unigram_feature_pos_tags=N,J 
     analyzer = ngram
     sim_threshold = 0.01
     k = 3

thesisgenerator/plugins/bov.py
@@ -185,6 +185,8 @@ class ThesaurusVectorizer(TfidfVectorizer):
             d = shelve.open(self.vector_source, flag='r') # read only
             self.vector_source = PrecomputedSimilaritiesVectorSource(Thesaurus(d))
 
+        self.ngram_range = [0, 0] # not interested in unigrams at decode time
+
         self.handler = get_token_handler(self.decode_token_handler,
                                          self.k,
                                          self.sim_compressor,

commit 254f845dbd16cb035b7bed21269846b91d7a1b47

Fixed what I broke in bd48d0a97.
I hardcoded a change in a parameter value at decode time, which broke many tests. This change is now a config file option and all code is backwards compatible.

conf/confrc
@@ -18,7 +18,7 @@ test_data=string(default='')
     strip_accents=string
     lowercase=boolean(default=False)
     token_pattern=string
-#     ngram_range=tuple() #ngram_range had to be in the format "1,3"
+#     ngram_range and ngram_range_decode --- have to be in the format "1,3". The decode version is used at decode time (if provided)- default is the same as encode-time
     min_df=integer
     max_df=integer
     analyser=string

conf/exp1-superbase.conf
@@ -12,6 +12,7 @@ joblib_caching = True
     stop_words = english
     min_df = 1
     ngram_range = 1,1
+	ngram_range_decode = 0,0 # # not interested in unigrams at decode time
     unigram_feature_pos_tags=N,J 
     analyzer = ngram
     sim_threshold = 0.01

thesisgenerator/plugins/bov.py
@@ -27,7 +27,9 @@ class ThesaurusVectorizer(TfidfVectorizer):
                  strip_accents=None,
                  preprocessor=None, tokenizer=None, analyzer='ngram',
                  stop_words=None, token_pattern=ur"(?u)\b\w\w+\b", min_n=None,
-                 max_n=None, ngram_range=(1, 1), max_df=1.0, min_df=2,
+                 max_n=None, ngram_range=(1, 1),
+                 ngram_range_decode=None,
+                 max_df=1.0, min_df=2,
                  max_features=None, vocabulary=None, binary=False, dtype=float,
                  norm='l2', use_idf=True, smooth_idf=True,
                  sublinear_tf=False, use_tfidf=True,
@@ -73,6 +75,7 @@ class ThesaurusVectorizer(TfidfVectorizer):
         self.unigram_feature_pos_tags = unigram_feature_pos_tags
         self.remove_features_with_NER = remove_features_with_NER
         self.random_neighbour_thesaurus = random_neighbour_thesaurus
+        self.ngram_range_decode = ngram_range_decode if ngram_range_decode else ngram_range
 
         self.stats = None
         self.handler = None
@@ -185,7 +188,7 @@ class ThesaurusVectorizer(TfidfVectorizer):
             d = shelve.open(self.vector_source, flag='r') # read only
             self.vector_source = PrecomputedSimilaritiesVectorSource(Thesaurus(d))
 
-        self.ngram_range = [0, 0] # not interested in unigrams at decode time
+        self.ngram_range = self.ngram_range_decode
 
         self.handler = get_token_handler(self.decode_token_handler,
                                          self.k,