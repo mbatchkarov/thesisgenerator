53219ed31ded4fa2f5ec5b71f0538725fe707c3f
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-04-08 16:45:51 +0100

    WIP: Now summarising (most) statistics over crossvalidation

--- thesisgenerator/scripts/signified_internals_analysis.py
+++ thesisgenerator/scripts/signified_internals_analysis.py
@@ -1,15 +1,17 @@
-from collections import Counter
+from collections import Counter, namedtuple
 import cPickle as pickle
+from itertools import chain
 from discoutils.tokens import DocumentFeature
 import matplotlib.pyplot as plt
 import pandas as pd
-from thesisgenerator.plugins.stats import StatsRecorder, sum_up_token_counts
+from thesisgenerator.plugins.stats import sum_up_token_counts
 from collections import defaultdict
+import numpy as np
 
 
 def histogram_from_list(l, path):
-    MAX_LABEL_COUNT = 20
-
+    MAX_LABEL_COUNT = 40
+    print 'Running histogram'
     plt.figure()
     if type(l[0]) == str:
         # numpy's histogram doesn't like strings
@@ -21,118 +23,151 @@ def histogram_from_list(l, path):
     plt.savefig(path, format='png')
 
 
-def get_basic_paraphrase_statistics(stats):
-    '''
-    Look at the kinds of replacements that are being made at decode time
-    :param stats:
-    :type stats: StatsRecorder
-    :return:
-    :rtype:
-    '''
-    replacement_count = [x.available_replacements for x in stats.paraphrases]
-    replacement_rank = [r for x in stats.paraphrases for r in x.ranks]
-    replacement_sims = [s for x in stats.paraphrases for s in x.similarities]
-    replacement_types = [feat.type for x in stats.paraphrases for feat in x.replacements]
-    return replacement_count, replacement_rank, replacement_sims, replacement_types
-
-
-def get_classificational_similarity_statistics(stats):
-    if hasattr(stats, 'ratio'):
-        replacement_ratios = {}
-        for p in stats.paraphras:
-            if p.original in stats.ratio:  # decode-time feature seen in training
-                original_value = stats.ratio[p.original]  # log class-conditional ratio
-                replacement_value = ([stats.ratio[r] for r in p.replacements])
-                print p, original_value, replacement_value
-
-    return replacement_ratios
-
-
-def do_work():
-    fname = 'stats-exp0-0-cv0-tr'
+def _train_time_counts(fname):
+    # BASIC STATISTICS AT TRAINING TIME
     df = sum_up_token_counts(fname)
     vocab = set(df.index.tolist())
-    print 'Vocabulary is %r' % list(vocab)
     df_it = df[df['IT'] > 0]
-    print 'Train-time IT tokens: %d/%d' % (df_it['count'].sum(), df['count'].sum())
-    print 'Train-time IT types: %d/%d' % (df_it.shape[0], df.shape[0])
-    fname = 'stats-exp0-0-cv0-ev'
+    return namedtuple('TrainCount', 'tokens, types, it_tokens, it_types')(tokens=df['count'].sum(),
+                                                                          types=df.shape[0],
+                                                                          it_tokens=df_it['count'].sum(),
+                                                                          it_types=df_it.shape[0])
+
+
+def _decode_time_counts(fname):
+    # BASIC STATISTICS AT DECODE TIME
     df = sum_up_token_counts(fname)
     df_it = df[df['IT'] > 0]
     df_iv = df[df['IV'] > 0]
-    print 'Decode-time IT tokens: %d/%d' % (df_it['count'].sum(), df['count'].sum())
-    print 'Decode-time IT types: %d/%d' % (df_it.shape[0], df.shape[0])
-    print 'Decode-time IV tokens: %d/%d' % (df_iv['count'].sum(), df['count'].sum())
-    print 'Decode-time IV types: %d/%d' % (df_iv.shape[0], df.shape[0])
-    print 'Decode-time IV IT tokens: %d' % (df[(df['IT'] > 0) & (df['IV'] > 0)]['count'].sum())
-    print 'Decode-time OOV IT tokens: %d' % (df[(df['IT'] == 0) & (df['IV'] > 0)]['count'].sum())
-    print 'Decode-time IV OOT tokens: %d' % (df[(df['IT'] > 0) & (df['IV'] == 0)]['count'].sum())
-    print 'Decode-time OOV OOT tokens: %d' % (df[(df['IT'] == 0) & (df['IV'] == 0)]['count'].sum())
-    df2 = pd.read_hdf(fname, 'paraphrases')
-    df2.columns = ('feature', 'available_replacements', 'max_replacements',
-                   'replacement1', 'replacement1_rank', 'replacement1_sim',
-                   'replacement2', 'replacement2_rank', 'replacement2_sim',
-                   'replacement3', 'replacement3_rank', 'replacement3_sim')
-    counts = df2.groupby('feature').count().feature
-    assert counts.sum() == df2.shape[0]  # no missing rows
-    df2 = df2.drop_duplicates()
-    df2.set_index('feature', inplace=True)
-    df2['count'] = counts
+
+    CountsResult = namedtuple('CountsResult', 'tokens types iv_types it_types iv it iv_it iv_oot oov_it oov_oot')
+    return CountsResult(
+        tokens=df['count'].sum(),
+        types=df.shape[0],
+        it=df_it['count'].sum(),
+        it_types=df_it.shape[0],
+        iv=df_iv['count'].sum(),
+        iv_types=df_iv.shape[0],
+        iv_it=df[(df['IT'] > 0) & (df['IV'] > 0)]['count'].sum(),
+        oov_it=df[(df['IT'] == 0) & (df['IV'] > 0)]['count'].sum(),
+        iv_oot=df[(df['IT'] > 0) & (df['IV'] == 0)]['count'].sum(),
+        oov_oot=df[(df['IT'] == 0) & (df['IV'] == 0)]['count'].sum()
+    )
+
+
+def _analyse_replacement_ranks_and_sims(df, fname):
+    # BASIC STATISTICS ABOUT REPLACEMENTS (RANK IN NEIGHBOURS LIST AND SIM OT ORIGINAL)
+    res = {}
+    for statistic in ['rank', 'sim']:
+        data = []
+        for i in range(1, 4):
+            values = df['replacement%d_%s' % (i, statistic)]
+            counts = df['count']
+            for value, count in zip(values, counts):
+                if value != 'NONE' and value != -1:  #
+                    data.extend([value] * count)
+        # histogram_from_list(data, 'figures/%s_%s_hist.png' % (fname, statistic))
+        res[statistic] = data
+    ReplacementsResult = namedtuple('ReplacementsResult', 'rank sim')
+    return ReplacementsResult(**res)
+
+
+def _analyse_replacements(fname):
+    df = pd.read_hdf(fname, 'paraphrases')
+    df.columns = ('feature', 'available_replacements', 'max_replacements',
+                  'replacement1', 'replacement1_rank', 'replacement1_sim',
+                  'replacement2', 'replacement2_rank', 'replacement2_sim',
+                  'replacement3', 'replacement3_rank', 'replacement3_sim')
+    counts = df.groupby('feature').count().feature
+    assert counts.sum() == df.shape[0]  # no missing rows
+    df = df.drop_duplicates()
+    df.set_index('feature', inplace=True)
+    df['count'] = counts
+
+    #####################################################################
+    # ANALYSE CLASS-CONDITIONAL PROBABILITY OF REPLACEMENTS
+    #####################################################################
 
     with open('%s.pickle' % fname) as infile:
         stats = pickle.load(infile)
-        print 1
-    # for i, stats in enumerate(stats_over_cv):
-    #     data = stats.get_paraphrase_statistics()
-    #     if not data:
-    #         continue  # stats may be disabled for performance reasons
-    #     for c, datatype in zip(data, ['count', 'rank', 'sim', 'feattype']):
-    #         histogram_from_list(c,
-    #                             'figures/%s_cv%d_%s_hist.png' % (fname, i, datatype))
-    #
     try:
         flp = stats.nb_feature_log_prob
-        # this only works for binary classifiers
+        # this only works for binary classifiers and IV-IT features
         ratio = flp[0, :] - flp[1, :]  # P(f|c0) / P(f|c1) in log space
         # high positive value mean strong association with class 0, very negative means the opposite
         scores = {feature: ratio[index] for index, feature in stats.nb_inv_voc.items()}
-        print scores
     except AttributeError:
         print 'Classifier parameters unavailable'
-        return
+        return None
 
     replacement_scores = defaultdict(int)
-    for f in df2.index:
+    for f in df.index:  # this contains all IT features, regardless of whether they were IV or IT
         try:
             orig_score = scores[DocumentFeature.from_string(f)]
         except KeyError:
-            # this feature was not in the original vocabulary, we inserted it
+            # this feature was not in the original vocabulary, we don't know it class conditional probs
             continue
 
-        repl_count = df2.ix[f]['count']
+        repl_count = df.ix[f]['count']
         if repl_count > 0:
             for i in range(1, 4):
-                replacement = df2.ix[f]['replacement%d' % i]
-                repl_sim = df2.ix[f]['replacement%d_sim' % i] # they should all be the same
+                replacement = df.ix[f]['replacement%d' % i]
+                repl_sim = df.ix[f]['replacement%d_sim' % i]  # they should all be the same
                 if repl_sim > 0:
                     # -1 signifies no replacement has been found
                     repl_score = repl_sim * scores[DocumentFeature.from_string(replacement)]
                     replacement_scores[(orig_score, repl_score)] += repl_count
-    x = []
-    y = []
-    thickness = []
-    print replacement_scores
-    for (orig_value, repl_value), repl_count in replacement_scores.iteritems():
-        y.append(repl_value)
-        x.append(orig_value)
-        thickness.append(repl_count)
-    print thickness
-    plt.scatter(x, y, thickness)
-    plt.hlines(0, min(x), max(x))
-    plt.vlines(0, min(y), max(y))
-    plt.xlabel('Class association of decode-time feature')
-    plt.ylabel('Class association of replacements')
-    plt.show()
+
+    return _analyse_replacement_ranks_and_sims(df, fname), replacement_scores
+
+
+def _print_counts_data(train_counts, title):
+    print '----------------------'
+    print '| %s time statistics:' % title
+    for field in train_counts[0]._fields:
+        print '| %s: mean %f, std %f' % ( field,
+                                        np.mean([x._asdict()[field] for x in train_counts]),
+                                        np.std([x._asdict()[field] for x in train_counts]))
+    print '----------------------'
+
+
+def do_work():
+    train_counts, decode_counts = [], []
+    basic_stats, replacement_scores = [], []
+    for cv_fold in [0, 1]:
+        fname = 'stats-exp0-0-cv%d-tr' % cv_fold
+        train_counts.append(_train_time_counts(fname))
+
+        fname = 'stats-exp0-0-cv%d-ev' % cv_fold
+        decode_counts.append(_decode_time_counts(fname))
+        a, b = _analyse_replacements(fname)
+        basic_stats.append(a)
+        replacement_scores.append(b)
+
+    # COLLATE AND AVERAGE STATS OVER CROSSVALIDATION
+    histogram_from_list(list(chain.from_iterable(x.rank for x in basic_stats)), 'stats-exp0-0-repl-ranks.png')
+    histogram_from_list(list(chain.from_iterable(x.sim for x in basic_stats)), 'stats-exp0-0-repl-sims.png')
+
+    _print_counts_data(train_counts, 'Train')
+    _print_counts_data(decode_counts, 'Decode')
+
+
+
+    if replacement_scores:
+        # sometimes there may not be any IV-IT features at decode time
+        x = []
+        y = []
+        thickness = []
+        for (orig_value, repl_value), repl_count in replacement_scores.iteritems():
+            y.append(repl_value)
+            x.append(orig_value)
+            thickness.append(repl_count)
+        plt.scatter(x, y, thickness)
+        plt.hlines(0, min(x), max(x))
+        plt.vlines(0, min(y), max(y))
+        plt.xlabel('Class association of decode-time feature')
+        plt.ylabel('Class association of replacements')
+        plt.show()
 
 
 if __name__ == '__main__':
