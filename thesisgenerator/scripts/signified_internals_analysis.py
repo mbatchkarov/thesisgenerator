dd22cb9cf91b1e6d72775cb8932d66899a2ed1d2
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-04-17 12:06:47 +0100

    Added a tiny bit more info to plots to make them self-contained

--- thesisgenerator/scripts/signified_internals_analysis.py
+++ thesisgenerator/scripts/signified_internals_analysis.py
@@ -227,13 +227,15 @@ def extract_stats_over_cv(subexp, cv_fold):
     return a, b, c, d, f, g
 
 
-def do_work(subexp, folds=25, workers=4):
+def do_work(exp, subexp, folds=25, workers=4, cursor=None):
     logging.info('---------------------------------------------------')
-    logging.info('Doing experiment %s', subexp)
-    plt.figure(figsize=(11, 8), dpi=300)  # for A4 print
-    plt.matplotlib.rcParams.update({'font.size': 9})
+    name = 'exp%d-%d' % (exp, subexp)
+    logging.info('Doing experiment %s', name)
 
-    res = Parallel(n_jobs=workers)(delayed(extract_stats_over_cv)(subexp, cv_fold) for cv_fold in range(folds))
+    plt.figure(figsize=(11, 8), dpi=300)  # for A4 print
+    plt.matplotlib.rcParams.update({'font.size': 8})
+
+    res = Parallel(n_jobs=workers)(delayed(extract_stats_over_cv)(name, cv_fold) for cv_fold in range(folds))
 
     train_counts = [x[0] for x in res]
     decode_counts = [x[1] for x in res]
@@ -262,17 +264,17 @@ def do_work(subexp, folds=25, workers=4):
     # sometimes there may not be any IV-IT features at decode time
     if it_iv_replacement_scores:
         # dump to disk so I can experiment with these counts later
-        with open('stats/%s-scores.pkl' % subexp, 'w') as outf:
+        with open('stats/%s-scores.pkl' % name, 'w') as outf:
             pickle.dump(it_iv_replacement_scores, outf)
 
-        plt.subplot(2, 3,  4)
+        plt.subplot(2, 3, 4)
         coef = plot_regression_line(*get_data(it_iv_replacement_scores))
         # Data currently rounded to 2 significant digits. Round to nearest int to make plot less cluttered
         myrange = plot_dots(round_scores_to_given_precision(it_iv_replacement_scores))
         plt.title('y=%.2fx%+.2f; w=%s--%s' % (coef[0], coef[1], myrange[0], myrange[1]))
 
     if it_iv_sim_vs_ratio:
-        plt.subplot(2, 3,  5)
+        plt.subplot(2, 3, 5)
         coef = plot_regression_line(*get_data(it_iv_sim_vs_ratio))
         myrange = plot_dots(round_scores_to_given_precision(it_iv_sim_vs_ratio, 1, 0),
                             xlabel='sim(a,b)',
@@ -280,8 +282,27 @@ def do_work(subexp, folds=25, workers=4):
                             draw_axes=False)
         plt.title('y=%.2fx%+.2f; w=%s--%s' % (coef[0], coef[1], myrange[0], myrange[1]))
 
+    if cursor:
+        plt.subplot(2, 3, 6)
+        query = 'SELECT score_mean, score_std FROM data%d WHERE NAME="%s" AND ' \
+                'metric="macroavg_f1" AND classifier="MultinomialNB"' % (exp, name)
+        cursor.execute(query)
+        f1 = 'F1=%.2f+-%1.2f' % cursor.fetchall()[0]
+
+        query = "SELECT * FROM ExperimentDescriptions WHERE number=%d" % exp
+        cursor.execute(query)
+        settings_data = cursor.fetchone()
+        desc = cursor.description
+        settings = {}
+
+        for (key, value) in zip(desc, settings_data):
+            settings[key[0]] = value
+
+        s = '\n'.join('%s:%s' % (str(k), str(v)) for k, v in settings.items())
+        plt.annotate('%s: %s\n%s' % (name, s, f1), (0.05, 0.5), textcoords='axes fraction')
+
     plt.tight_layout()
-    plt.savefig('figures/stats-%s.png' % subexp, format='png')
+    plt.savefig('figures/stats-%s.png' % name, format='png')
 
 
 if __name__ == '__main__':
@@ -289,8 +310,9 @@ if __name__ == '__main__':
                         filename='figures/stats_output.txt',
                         filemode='w',
                         format="%(asctime)s\t%(module)s.%(funcName)s ""(line %(lineno)d)\t%(levelname)s : %(message)s")
+    from thesisgenerator.utils.misc import get_susx_mysql_conn
 
-    do_work(subexp='exp0-0', folds=2, workers=1)
-    do_work('exp1-10', folds=4)
-    do_work('exp3-10', folds=4)
+    c = get_susx_mysql_conn().cursor()
+    for i in range(1, 45):
+        do_work(i, 5, folds=20, workers=5, cursor=c)
 
