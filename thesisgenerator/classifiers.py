Switched to using string labels throughout for clarity. Sklearn metrics weren't doing what I though they were (sklearn issue 3122). This is a temp fix to align my understanding of macro/micro averaging with what sklearn does, until their docs are updated

thesisgenerator/classifiers.py
@@ -190,7 +190,9 @@ class SubsamplingPredefinedIndicesIterator(object):
         self.num_samples = num_samples
         self.sample_size = int(sample_size)
         self.rng = check_random_state(random_state)
-        self.counts = np.bincount(y_vals) / float(len(y_vals))
+        self.counts = Counter(y_vals)
+        for label, freq in self.counts.items():
+            self.counts[label] /= float(len(y_vals))
         logging.info('Will do %d runs, for each sampling %d documents from a training set of size %d',
                      self.num_samples,
                      self.sample_size,
@@ -200,7 +202,7 @@ class SubsamplingPredefinedIndicesIterator(object):
         for i in range(self.num_samples):
             ind_train = np.zeros((0,), dtype=np.int)
 
-            for label, proportion in enumerate(list(self.counts)):
+            for label, proportion in self.counts.items():
                 train_size = int(round(proportion * self.sample_size))
 
                 ind = np.nonzero(self.y_vals[self.train] == label)[0]

thesisgenerator/utils/data_utils.py
@@ -1,8 +1,9 @@
 import logging
 import os
-from discoutils.misc import ContainsEverything
-from joblib import Memory
 import random
+import numpy as np
+from joblib import Memory
+from discoutils.misc import ContainsEverything
 from sklearn.datasets import load_files
 from thesisgenerator.classifiers import NoopTransformer
 from thesisgenerator.composers.vectorstore import UnigramVectorSource, CompositeVectorSource, \
@@ -83,9 +84,8 @@ def _get_data_iterators(path, shuffle_targets=False):
     if shuffle_targets:
         logging.warn('RANDOMIZING TARGETS')
         random.shuffle(dataset.target)
-    targets_iterable = dataset.target
 
-    return data_iterable, targets_iterable
+    return data_iterable, np.array(dataset.target_names)[dataset.target]
 
 
 def get_vector_source(conf, vector_source=None):