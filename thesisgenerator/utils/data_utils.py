2990780938f8b66cae79f6f118ea7ac569f14975
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-05-02 12:40:02 +0100

    Added function to pre-shelf all thesauri needed for a set of experiments

--- thesisgenerator/utils/data_utils.py
+++ thesisgenerator/utils/data_utils.py
@@ -1,17 +1,23 @@
+import argparse
+from glob import glob
 import logging
 import os
 import random
+import sys
+
+sys.path.append('.')
+sys.path.append('..')
+sys.path.append('../..')
+from discoutils.thesaurus_loader import Thesaurus
 import numpy as np
-from joblib import Memory
+from joblib import Memory, Parallel, delayed
 from discoutils.misc import ContainsEverything
 from sklearn.datasets import load_files
 from thesisgenerator.classifiers import NoopTransformer
-from thesisgenerator.composers.vectorstore import UnigramVectorSource, CompositeVectorSource, \
-    PrecomputedSimilaritiesVectorSource
+from thesisgenerator.composers.vectorstore import UnigramVectorSource, CompositeVectorSource
 from thesisgenerator.plugins import tokenizers
+from thesisgenerator.utils.conf_file_utils import parse_config_file
 from thesisgenerator.utils.reflection_utils import get_named_object, get_intersection_of_parameters
-
-__author__ = 'mmb28'
 
 
 def tokenize_data(data, tokenizer, corpus_ids):
@@ -128,26 +134,81 @@ def get_vector_source(conf, vector_source=None):
                     conf['vector_sources']['include_self'],
                 )
         else:
-            filename = 'shelf%d' % hash(tuple(paths))
-            if not os.path.exists(filename):
-                logging.info('Loading precomputed neighbour sources')
-                entry_types_to_load = conf['vector_sources']['entry_types_to_load']
-                if not entry_types_to_load:
-                    entry_types_to_load = ContainsEverything()
+            logging.info('Loading precomputed neighbour source')
+            entry_types_to_load = conf['vector_sources']['entry_types_to_load']
+            if not entry_types_to_load:
+                entry_types_to_load = ContainsEverything()
 
-                vector_source = PrecomputedSimilaritiesVectorSource.from_file(
-                    thesaurus_files=paths,
-                    sim_threshold=conf['vector_sources']['sim_threshold'],
-                    include_self=conf['vector_sources']['include_self'],
-                    allow_lexical_overlap=conf['vector_sources']['allow_lexical_overlap'],
-                    max_neighbours=conf['vector_sources']['max_neighbours'],
-                    row_filter=lambda x, y: y.type in entry_types_to_load
-                )
-                vector_source.th.to_shelf(filename)
-            vector_source = filename
+            vector_source = load_and_shelve_thesaurus(paths,
+                                                      conf['vector_sources']['sim_threshold'],
+                                                      conf['vector_sources']['include_self'],
+                                                      conf['vector_sources']['allow_lexical_overlap'],
+                                                      conf['vector_sources']['max_neighbours'],
+                                                      entry_types_to_load)
     else:
         if not vector_source:
             # if a vector source has not been passed in and has not been initialised, then init it to avoid
             # accessing empty things
+            logging.warn('RETURNING AN EMPTY VECTOR SOURCE')
             vector_source = []
     return vector_source
+
+
+def load_and_shelve_thesaurus(files, sim_threshold, include_self,
+                              allow_lexical_overlap, max_neighbours, entry_types_to_load):
+    """
+    Parses and then shelves a thesaurus file. Reading from it is much faster and memory efficient than
+    keeping it in memory. Returns the path to the shelf file
+    """
+    filename = 'shelf%d' % hash(tuple(files))
+    if os.path.exists(filename):
+        logging.info('Returning pre-shelved object %s for %s', filename, files)
+    else:
+        th = Thesaurus.from_tsv(thesaurus_files=files,
+                                sim_threshold=sim_threshold,
+                                include_self=include_self,
+                                allow_lexical_overlap=allow_lexical_overlap,
+                                max_neighbours=max_neighbours,
+                                row_filter=lambda x, y: y.type in entry_types_to_load)
+        logging.info('Shelving %s', files)
+        th.to_shelf(filename)
+    return filename
+
+
+def _do_work(th, conf):  # should really be a lambda/closure but joblib doesn't work with lambdas
+    entry_types_to_load = conf['vector_sources']['entry_types_to_load']
+    if not entry_types_to_load:
+        entry_types_to_load = ContainsEverything()
+
+    if os.path.exists(th):
+        load_and_shelve_thesaurus([th],
+                                  conf['vector_sources']['sim_threshold'],
+                                  conf['vector_sources']['include_self'],
+                                  conf['vector_sources']['allow_lexical_overlap'],
+                                  conf['vector_sources']['max_neighbours'],
+                                  entry_types_to_load)
+    else:
+        logging.warn('Thesaurus does not exist: %s', th)
+
+
+def shelve_all_thesauri(n_jobs):
+    """
+    Loads, parses and shelves all thesauri used in experiments.
+    :return:
+    """
+    conf_files = glob('conf/exp*/exp*_base.conf')
+    thesauri = set(parse_config_file(path)[0]['vector_sources']['unigram_paths'][0] for path in conf_files)
+    conf, _ = parse_config_file('conf/exp1-superbase.conf')
+
+    Parallel(n_jobs=n_jobs)(delayed(_do_work)(th, conf) for th in thesauri)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s\t%(module)s.%(funcName)s (line %(lineno)d)\t%(levelname)s : %("
+                               "message)s")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('jobs', type=int)
+    shelve_all_thesauri(parser.parse_args().jobs)
+
