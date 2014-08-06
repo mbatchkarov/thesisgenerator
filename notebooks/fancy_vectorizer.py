b16c416cb6e4aeaea6084c55fdb551f0a2eb1142
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-08-06 11:44:36 +0100

    Sped up euroscipy demo using a ball tree

--- notebooks/fancy_vectorizer.py
+++ notebooks/fancy_vectorizer.py
@@ -1,34 +1,45 @@
 from collections import defaultdict
 import array
 import logging
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import numpy as np
 import scipy.sparse as sp
+from sklearn.neighbors import NearestNeighbors
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.metrics import f1_score, accuracy_score
+from functools import partial, lru_cache
+from operator import itemgetter
+import gensim
 
 
-class DistributionalVectorizer(TfidfVectorizer):
+class DistributionalVectorizer(CountVectorizer):
     """
     A simplified version of thesisgenerator's ThesaurusVectorizer for EuroScipy talk
     Assumes hybrid feature encoding, backed by a word2vec model
 
     """
-    # todo this class is painfully slow, see ./fancyvect.prof
     def __init__(self, model):
         self.thesaurus = model
-        self.k = 1  # todo hardcoded value
+        self.k = 3  # todo hardcoded value
         super().__init__()  # todo not passing on any parameters
 
     def paraphrase(self, feature, vocabulary, j_indices, values, **kwargs):
-        neighbours = self.thesaurus.most_similar(feature)
+        neighbours = self.get_nearest_neighbours(feature)
         neighbours = [foo for foo in enumerate(neighbours) if foo[0] in vocabulary]
-
         for neighbour, sim in neighbours[:self.k]:
             j_indices.append(vocabulary.get(neighbour))
-            values.append(sim)  # todo using raw similarity
+            values.append(1.)  # todo not using similarity
 
     def insert_feature_only(self, feature_index_in_vocab, j_indices, values, **kwargs):
         j_indices.append(feature_index_in_vocab)
         values.append(1)
+
+    def fit_transform(self, raw_documents, y=None):
+        res = super().fit_transform(raw_documents, y)
+        self.init_sims(self.vocabulary_.keys())
+        return res
 
     def _count_vocab(self, raw_documents, fixed_vocab):
         """
@@ -99,31 +110,63 @@ class DistributionalVectorizer(TfidfVectorizer):
         X.sum_duplicates()  # nice that the summation is explicit
         return vocabulary, X
 
+    def init_sims(self, vocab, n_neighbors=10):
+        # self.name2row = {word: i for (i, word) in enumerate(self.thesaurus.index2word)}
+        # selected_rows = [self.name2row[foo] for foo in vocab if foo in self.name2row]
+        # if not selected_rows:
+        # raise ValueError('None of the vocabulary items in the labelled set have associated vectors')
+        # row2name = {v: k for k, v in self.name2row.items()}
+        # self.selected_row2name = {new: row2name[old] for new, old in enumerate(selected_rows)}
+        self.n_neighbours = n_neighbors
+
+        self.selected_row2name = [f for f in vocab if f in self.thesaurus]
+        matrix = np.vstack((self.thesaurus[f] for f in self.selected_row2name))
+        num_vectors = matrix.shape[1]
+        if n_neighbors > num_vectors:
+            logging.warning('You requested %d neighbours to be returned, but there are only %d. Truncating.',
+                            n_neighbors, num_vectors)
+            n_neighbors = num_vectors - 1
+
+        # BallTree/KDTree do not support cosine out of the box. algorithm='brute' may be slower overall
+        # it's faster to build ,O(1), and slower to query
+        self.nn = NearestNeighbors(algorithm='ball_tree',
+                                   metric='euclidean',  # todo using euclidean instead of cosine with ball tree
+                                   n_neighbors=n_neighbors + 1).fit(matrix)
+        self.get_nearest_neighbours.cache_clear()
+
+    @lru_cache(maxsize=2 ** 13)
+    def get_nearest_neighbours(self, entry):
+        """
+        Get the nearest neighbours of `entry` amongst all entries that `init_sims` was called with. The top
+        neighbour will never be the entry itself (to match Byblo's behaviour)
+        """
+        if entry not in self.thesaurus:
+            raise ValueError('%s should have been in the models' % entry)
+
+        distances, indices = self.nn.kneighbors(self.thesaurus[entry])
+        neigh = [(self.selected_row2name[indices[0][i]], distances[0][i]) for i in range(indices.shape[1])]
+        if neigh[0][0] == entry:
+            neigh.pop(0)
+        return neigh[:self.n_neighbours]
+
 
 def eval_classifier(DATA_SIZES, NUM_CV, newsgroups_train, newsgroups_test, vect_callable=TfidfVectorizer):
     accuracy = np.zeros((len(DATA_SIZES), NUM_CV))
     for i, train_size in enumerate(DATA_SIZES):
         cv_iter = ShuffleSplit(len(newsgroups_train.data), n_iter=NUM_CV, train_size=train_size)
         for j, (train_idx, _) in enumerate(cv_iter):
+            print('Size %d, fold %d' % (train_size, j))
             vectorizer = vect_callable()
             clf = MultinomialNB(alpha=.001)
-            tr = vectorizer.fit_transform(np.array(newsgroups_train.data)[train_idx])
+            tr = vectorizer.fit_transform(itemgetter(*train_idx)(newsgroups_train.data))
             clf = clf.fit(tr, newsgroups_train.target[train_idx])
-            ev = vectorizer.transform(newsgroups_test.data[:10]) # todo remove slice
-            score = accuracy_score(newsgroups_test.target[:10], clf.predict(ev)) # todo remove slice
+            ev = vectorizer.transform(newsgroups_test.data)
+            score = accuracy_score(newsgroups_test.target, clf.predict(ev))
             accuracy[i, j] = score
     return accuracy
 
 
 if __name__ == '__main__':
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    from sklearn.naive_bayes import MultinomialNB
-    from sklearn.datasets import fetch_20newsgroups
-    from sklearn.cross_validation import ShuffleSplit
-    from sklearn.metrics import f1_score, accuracy_score
-    from functools import partial
-    import gensim
-
     model = gensim.models.Word2Vec.load('ass.word2vec.model')
     p = partial(DistributionalVectorizer, model)
 
