f9c8d02073c76f6e44cd17a34325f3b22e907ed6
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-10-22 17:36:15 +0100

    Added size of gziped version to vectors DB
    Good smoke test for missing gz files

--- thesisgenerator/utils/db.py
+++ thesisgenerator/utils/db.py
@@ -23,6 +23,7 @@ class Vectors(pw.Model):
 
     modified = pw.DateField(null=True, default=None)  # when was the file last modifier
     size = pw.IntegerField(null=True, default=None)  # file size in MB
+    gz_size = pw.IntegerField(null=True, default=None)  # file size of compressed JSON version
 
     class Meta:
         database = db
