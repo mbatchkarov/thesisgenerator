5713b5cd73f3730814e423d9054af3e74cfb85d5
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-11-24 14:06:08 +0000

    wrapped main part in if __name__ == '__main__'

--- thesisgenerator/scripts/generate_classification_conf_files.py
+++ thesisgenerator/scripts/generate_classification_conf_files.py
@@ -161,106 +161,107 @@ def random_vectors_on_r2():
     experiments.append(e)
 
 
-prefix = '/mnt/lustre/scratch/inf/mmb28/thesisgenerator/sample-data'
-techtc_corpora = sorted(list(os.path.join(*x.split(os.sep)[-2:]) \
-                             for x in glob('%s/techtc100-clean/*' % prefix) \
-                             if not x.endswith('.gz')))
-r2_corpus = 'reuters21578/r8-tagged-grouped'
-mr_corpus = 'movie-reviews-tagged'
-am_corpus = 'amazon_grouped-tagged'
-all_corpora = techtc_corpora + [r2_corpus, mr_corpus, am_corpus]
+if __name__ == '__main__':
+    prefix = '/mnt/lustre/scratch/inf/mmb28/thesisgenerator/sample-data'
+    techtc_corpora = sorted(list(os.path.join(*x.split(os.sep)[-2:]) \
+                                 for x in glob('%s/techtc100-clean/*' % prefix) \
+                                 if not x.endswith('.gz')))
+    r2_corpus = 'reuters21578/r8-tagged-grouped'
+    mr_corpus = 'movie-reviews-tagged'
+    am_corpus = 'amazon_grouped-tagged'
+    all_corpora = techtc_corpora + [r2_corpus, mr_corpus, am_corpus]
 
-db.ClassificationExperiment.raw('TRUNCATE TABLE `classificationexperiment`;')
-experiments = []
-baselines()
-all_standard_experiments()
-hybrid_experiments()
-use_similarity_experiments()
-an_only_nn_only_experiments_r2()
-word2vec_with_less_data_on_r2(range(10, 91, 10))
-word2vec_repeats_on_r2()
-glove_vectors_r2()
-word2vec_with_less_data_on_r2(range(1, 10, 1))  # these were added later
-random_vectors_on_r2()
-word2vec_with_less_data_on_r2(np.arange(0.01, 0.92, .1))
+    db.ClassificationExperiment.raw('TRUNCATE TABLE `classificationexperiment`;')
+    experiments = []
+    baselines()
+    all_standard_experiments()
+    hybrid_experiments()
+    use_similarity_experiments()
+    an_only_nn_only_experiments_r2()
+    word2vec_with_less_data_on_r2(range(10, 91, 10))
+    word2vec_repeats_on_r2()
+    glove_vectors_r2()
+    word2vec_with_less_data_on_r2(range(1, 10, 1))  # these were added later
+    random_vectors_on_r2()
+    word2vec_with_less_data_on_r2(np.arange(0.01, 0.92, .1))
 
-# re-order experiments so that the hard ones (high-memory, long-running) come first
-def _myorder(item):
-    """
-    If the thing is AM move it to the from of the sorted list, leave
-    all other items in the order they were in
-    :param item:
-    :return:
-    """
-    lab = getattr(item[1], 'labelled')
-    if 'amazon' in lab:
-        return chr(1), item[0]
-    return 'whatever', item[0]
+    # re-order experiments so that the hard ones (high-memory, long-running) come first
+    def _myorder(item):
+        """
+        If the thing is AM move it to the from of the sorted list, leave
+        all other items in the order they were in
+        :param item:
+        :return:
+        """
+        lab = getattr(item[1], 'labelled')
+        if 'amazon' in lab:
+            return chr(1), item[0]
+        return 'whatever', item[0]
 
 
-sorted_experiments = sorted(enumerate(experiments), key=_myorder)
-experiments = []
-for new_id, (old_id, e) in enumerate(sorted_experiments, 1):
-    e.id = new_id
-    experiments.append(e)
-for e in experiments:
-    e.save(force_insert=True)
-    print('%s,' % e)
+    sorted_experiments = sorted(enumerate(experiments), key=_myorder)
+    experiments = []
+    for new_id, (old_id, e) in enumerate(sorted_experiments, 1):
+        e.id = new_id
+        experiments.append(e)
+    for e in experiments:
+        e.save(force_insert=True)
+        print('%s,' % e)
 
-# sys.exit(0)
-print('Writing conf files')
-megasuperbase_conf_file = 'conf/exp1-superbase.conf'
-for exp in experiments:
-    if exp.id % 20 == 0:
-        print('Writing exp %d' % exp.id)
-    # sanity check
-    experiment_dir = 'conf/exp%d' % exp.id
-    if not os.path.exists(experiment_dir):
-        os.mkdir(experiment_dir)
+    # sys.exit(0)
+    print('Writing conf files')
+    megasuperbase_conf_file = 'conf/exp1-superbase.conf'
+    for exp in experiments:
+        if exp.id % 20 == 0:
+            print('Writing exp %d' % exp.id)
+        # sanity check
+        experiment_dir = 'conf/exp%d' % exp.id
+        if not os.path.exists(experiment_dir):
+            os.mkdir(experiment_dir)
 
-    base_conf_file = os.path.join(experiment_dir, 'exp%d_base.conf' % exp.id)
-    conf, _ = parse_config_file(megasuperbase_conf_file)
+        base_conf_file = os.path.join(experiment_dir, 'exp%d_base.conf' % exp.id)
+        conf, _ = parse_config_file(megasuperbase_conf_file)
 
-    if exp.vectors is None and 'Base' in exp.decode_handler:
-        # signifier baseline, not using a thesaurus, so shouldn't do any feature selection based on the thesaurus
-        conf['feature_selection']['must_be_in_thesaurus'] = False
+        if exp.vectors is None and 'Base' in exp.decode_handler:
+            # signifier baseline, not using a thesaurus, so shouldn't do any feature selection based on the thesaurus
+            conf['feature_selection']['must_be_in_thesaurus'] = False
 
-    conf['training_data'] = os.path.join(prefix, exp.labelled)
-    conf['feature_extraction']['decode_token_handler'] = \
-        'thesisgenerator.plugins.bov_feature_handlers.%s' % exp.decode_handler
-    conf['feature_extraction']['random_neighbour_thesaurus'] = \
-        exp.vectors is not None and exp.vectors.algorithm == 'random_neigh'
-    conf['vector_sources']['neighbours_file'] = exp.vectors.path if exp.vectors else ''
-    conf['output_dir'] = './conf/exp%d/output' % exp.id
-    conf['name'] = 'exp%d' % exp.id
+        conf['training_data'] = os.path.join(prefix, exp.labelled)
+        conf['feature_extraction']['decode_token_handler'] = \
+            'thesisgenerator.plugins.bov_feature_handlers.%s' % exp.decode_handler
+        conf['feature_extraction']['random_neighbour_thesaurus'] = \
+            exp.vectors is not None and exp.vectors.algorithm == 'random_neigh'
+        conf['vector_sources']['neighbours_file'] = exp.vectors.path if exp.vectors else ''
+        conf['output_dir'] = './conf/exp%d/output' % exp.id
+        conf['name'] = 'exp%d' % exp.id
 
-    requested_features = exp.document_features.split('_')
-    for doc_feature_type in ['AN', 'NN', 'VO', 'SVO']:
-        conf['feature_extraction']['extract_%s_features' % doc_feature_type] = doc_feature_type in requested_features
+        requested_features = exp.document_features.split('_')
+        for doc_feature_type in ['AN', 'NN', 'VO', 'SVO']:
+            conf['feature_extraction']['extract_%s_features' % doc_feature_type] = doc_feature_type in requested_features
 
-    # do not allow lexical overlap to prevent Left and Right from relying on word identity
-    conf['vector_sources']['allow_lexical_overlap'] = False
+        # do not allow lexical overlap to prevent Left and Right from relying on word identity
+        conf['vector_sources']['allow_lexical_overlap'] = False
 
-    if exp.use_similarity:
-        conf['feature_extraction']['sim_compressor'] = 'thesisgenerator.utils.misc.unit'
-    else:
-        conf['feature_extraction']['sim_compressor'] = 'thesisgenerator.utils.misc.one'
+        if exp.use_similarity:
+            conf['feature_extraction']['sim_compressor'] = 'thesisgenerator.utils.misc.unit'
+        else:
+            conf['feature_extraction']['sim_compressor'] = 'thesisgenerator.utils.misc.one'
 
-    with open(base_conf_file, 'wb') as inf:
-        conf.write(inf)
+        with open(base_conf_file, 'wb') as inf:
+            conf.write(inf)
 
-    # we've just written a conf file to a directory that may have contained results from before
-    # check those results were done with the same configuration we just created. This is needed
-    # because as we reorder experiments existing results may end up with a different ID
-    # otherwise the experiment will have to be re-run
-    # only checking some of the important parameters
-    previous_conf_file = 'conf/exp{0}/exp{0}_base-variants/exp{0}-0.conf'.format(exp.id)
-    if os.path.exists(previous_conf_file):
-        old_conf, _ = parse_config_file(previous_conf_file)
-        for a, b in [(old_conf['vector_sources']['neighbours_file'],
-                      conf['vector_sources']['neighbours_file']),
-                     (old_conf['feature_extraction']['decode_token_handler'],
-                      conf['feature_extraction']['decode_token_handler']),
-                     (conf['training_data'], old_conf['training_data'])]:
-            if a != b:
-                print('Exp: %d, was %r, is now %r' % (exp.id, a, b))
+        # we've just written a conf file to a directory that may have contained results from before
+        # check those results were done with the same configuration we just created. This is needed
+        # because as we reorder experiments existing results may end up with a different ID
+        # otherwise the experiment will have to be re-run
+        # only checking some of the important parameters
+        previous_conf_file = 'conf/exp{0}/exp{0}_base-variants/exp{0}-0.conf'.format(exp.id)
+        if os.path.exists(previous_conf_file):
+            old_conf, _ = parse_config_file(previous_conf_file)
+            for a, b in [(old_conf['vector_sources']['neighbours_file'],
+                          conf['vector_sources']['neighbours_file']),
+                         (old_conf['feature_extraction']['decode_token_handler'],
+                          conf['feature_extraction']['decode_token_handler']),
+                         (conf['training_data'], old_conf['training_data'])]:
+                if a != b:
+                    print('Exp: %d, was %r, is now %r' % (exp.id, a, b))
