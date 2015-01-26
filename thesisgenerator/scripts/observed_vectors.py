af88233901885fe639a1e0fbfe1974b36a783da8
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-01-26 10:16:10 +0000

    improved bits and pieces

--- thesisgenerator/scripts/observed_vectors.py
+++ thesisgenerator/scripts/observed_vectors.py
@@ -1,3 +1,5 @@
+import argparse
+import logging
 from os.path import join, dirname
 from discoutils.cmd_utils import run_and_log_output
 from discoutils.misc import temp_chdir, mkdirs_if_not_exists
@@ -7,47 +9,60 @@ Implements the Evernote note "Training Baroni composers for NP experiments"
 Jumps throught a bunch of hoops to extract observed window vectors for NPs
 List of phrases in labelled corpus must have been extracted already
  """
-# todo this contains a number of hardcoded thresholds
-# todo all temporary paths should be marked as such
-# todo final output file naming is a mess to comply with old convention, could be neater
-# todo need to parameterize for gigaw/wikipedia
+# this produces a bunch of all temporary files. Should they be marked as such?
+# not all steps steps handle empty inputs well
+# if any crashes with a weird error, it's likely the filtering thresholds are too high
 
-prefix = '/mnt/lustre/scratch/inf/mmb28/'
-byblo_base_dir = join(prefix, 'FeatureExtrationToolkit/', 'Byblo-2.2.0')
-discoutils = join(prefix, 'DiscoUtils')
-modifiers = join(prefix, 'thesisgenerator',
-                         'NPs_in_R2_MR_tech_am_maas',
-                         'r2-mr-technion-am-maas-modifiers.txt')
 
-# Find all NPs in unlabelled corpus whose modifier is in labelled corpus,
-# and which appear more than thresh=100 times in unlabelled corpus
-thresh = '100'  # this needs to be a str, otherwise iterpipes breaks
-script = join(discoutils, 'discoutils', 'find_all_NPs.py')
-byblo_features_file = join(prefix, 'FeatureExtrationToolkit',
-                                   'feoutput-deppars', 'exp10')
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s\t%(module)s.%(funcName)s (line %(lineno)d)\t%(levelname)s : %(message)s")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--corpus', choices=('gigaw', 'wiki'), required=True)
+    args = parser.parse_args()
 
-out1 = join(discoutils, 'gigaw_NPs_in_MR_R2_TechTC_am_maas.txt')
-out2 = join(discoutils, 'gigaw_NPs_in_MR_R2_TechTC_am_maas.uniq.100.txt')
-out3 = join(discoutils, 'gigaw-obs-wins', 'gigaw-obs-wins.fet')
+    features_file = 'exp10' if args.corpus == 'gigaw' else 'exp11'  # the output of FET
+    prefix = '/mnt/lustre/scratch/inf/mmb28/'
+    byblo_base_dir = join(prefix, 'FeatureExtrationToolkit/', 'Byblo-2.2.0')
+    discoutils = join(prefix, 'DiscoUtils')
+    modifiers = join(prefix, 'thesisgenerator',
+                     'NPs_in_R2_MR_tech_am_maas',
+                     'r2-mr-technion-am-maas-modifiers.txt')
+    byblo_filter_thresholds = 200, 200, 100
+    min_corpus_freq, min_features = 100, 10
 
-with temp_chdir(discoutils):
-    cmd = 'python {} {} -o {} -s {}'
-    run_and_log_output(cmd, script, byblo_features_file, out1, modifiers)
+    # Find all NPs in unlabelled corpus whose modifier is in labelled corpus,
+    # and which appear more than thresh=100 times in unlabelled corpus
+    script = join(discoutils, 'discoutils', 'find_all_NPs.py')
+    byblo_features_file = join(prefix, 'FeatureExtrationToolkit',
+                               'feoutput-deppars', features_file)
+    out1 = join(discoutils, '%s_NPs_in_MR_R2_TechTC_am_maas.txt' % args.corpus)
+    out2 = join(discoutils, '%s_NPs_in_MR_R2_TechTC_am_maas.uniq.%d.txt' % (args.corpus, min_corpus_freq))
+    out3 = join(discoutils,
+                '%s-obs-wins' % args.corpus,
+                '%s-obs-wins.fet' % args.corpus)
 
-    run_and_log_output("cat {} | sort | uniq -c | awk '$1>{} {print $2}' > {}", out1, thresh, out2)
+    with temp_chdir(discoutils):
+        cmd = 'python {} {} -o {} -s {}'
+        run_and_log_output(cmd, script, byblo_features_file, out1, modifiers)
 
-    mkdirs_if_not_exists(join(discoutils, 'gigaw-obs-wins'))
-    run_and_log_output('python discoutils/find_all_NPs.py {} -v -s {} -o {}',
-                       byblo_features_file, out2, out3)
+        run_and_log_output("cat {} | sort | uniq -c | awk '$1>{} {print $2}' > {}",
+                           out1, str(min_corpus_freq), out2)
 
-with temp_chdir(byblo_base_dir):
-    run_and_log_output('./byblo.sh -i {} -o {} -t 10 --stages enumerate,count,filter '
-                       '--filter-entry-freq 200 --filter-feature-freq 200 --filter-event-freq 100',
-                       out3, dirname(out3))
-    run_and_log_output('./unindex-all.sh {}', out3)
+        mkdirs_if_not_exists(join(discoutils,
+                                  '%s-obs-wins' % args.corpus))
+        run_and_log_output('python discoutils/find_all_NPs.py {} -v -s {} -o {}',
+                           byblo_features_file, out2, out3)
 
-with temp_chdir(dirname(out3)):
-    obs_vectors_dir = join(prefix, 'FeatureExtractionToolkit', 'observed_vectors')
-    mkdirs_if_not_exists(obs_vectors_dir)
-    run_and_log_output("awk 'NF>11' gigaw-obs-wins.fet.events.filtered.strings >  {}",
-                       join(obs_vectors_dir, 'exp10-13_AN_NNvectors-cleaned'))
+    with temp_chdir(byblo_base_dir):
+        run_and_log_output('./byblo.sh -i {} -o {} -t 10 --stages enumerate,count,filter '
+                           '--filter-entry-freq {} --filter-feature-freq {} --filter-event-freq {}',
+                           out3, dirname(out3), *map(str, byblo_filter_thresholds))
+        run_and_log_output('./unindex-all.sh {}', out3)
+
+    with temp_chdir(dirname(out3)):
+        obs_vectors_dir = join(prefix, 'FeatureExtractionToolkit', 'observed_vectors')
+        mkdirs_if_not_exists(obs_vectors_dir)
+        run_and_log_output("awk 'NF>{}' {}-obs-wins.fet.events.filtered.strings >  {}",
+                           str(min_features), args.corpus,
+                           join(obs_vectors_dir, '%s_NPs_wins_observed' % args.corpus))
