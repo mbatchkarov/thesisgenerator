c7ec537de1cdc7cfe48218c19358c05b530e7422
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-04-28 19:43:51 +0100

    changed threshold
    observed vectors have lots of noise and stopwords

--- thesisgenerator/scripts/observed_vectors.py
+++ thesisgenerator/scripts/observed_vectors.py
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     TG = join(prefix, 'thesisgenerator')
     byblo_base_dir = join(prefix, 'FeatureExtractionToolkit/', 'Byblo-2.2.0')
     discoutils = join(prefix, 'DiscoUtils')
-    byblo_filter_thresholds = 100, 100, 50
-    min_corpus_freq, min_features = 50, 5
+    byblo_filter_thresholds = 100, 100, 10
+    min_corpus_freq, min_features = 50, 9 # num_fields_in_line = 1 + 2*num_features
 
     # Find all NPs in unlabelled corpus whose modifier is in labelled corpus,
     # and which appear more than thresh=100 times in unlabelled corpus
