7bca555d0362cb5db665572bf3f65234ae837b82
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-07-16 12:39:29 +0100

    revamped turian/socher vectors code
    explicitly saving all unigram embeddings, with better handling of missing
    PoS tags. See docstring

--- thesisgenerator/scripts/socher_vectors.py
+++ thesisgenerator/scripts/socher_vectors.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+import string
 import sys
+from nltk import WordNetLemmatizer
 
 sys.path.append('.')
 import argparse
@@ -6,11 +9,14 @@ import logging
 from operator import itemgetter
 import os
 import numpy as np
+import pandas as pd
 import scipy.sparse as sp
+from scipy.io import loadmat
 from discoutils.tokens import DocumentFeature
 from discoutils.io_utils import write_vectors_to_hdf
 from discoutils.cmd_utils import run_and_log_output
 from discoutils.misc import temp_chdir, mkdirs_if_not_exists
+from discoutils.thesaurus_loader import DenseVectors
 from thesisgenerator.composers.vectorstore import (MultiplicativeComposer, AdditiveComposer,
                                                    RightmostWordComposer, LeftmostWordComposer,
                                                    VerbComposer, compose_and_write_vectors)
@@ -33,11 +39,13 @@ plaintext_socher_input_file = os.path.join(prefix, '..', 'thesisgenerator',
 
 socher_output_phrases_file = os.path.join(socher_base_dir, 'phrases.txt')
 socher_output_vectors_file = os.path.join(socher_base_dir, 'outVectors.txt')
+socher_unigram_embedding_matlab = os.path.join(socher_base_dir, 'vars.normalized.100.mat')
 
 # output of reformat stage
+turian_unigram_vectors_file = os.path.join(socher_base_dir, 'turian_unigrams.h5')
 output_dir = os.path.join(socher_base_dir, 'composed')
 mkdirs_if_not_exists(output_dir)
-vectors_file = os.path.join(output_dir, 'AN_NN_turian_Socher.events.filtered.strings')
+socher_composed_vectors_file = os.path.join(output_dir, 'AN_NN_turian_Socher.events.filtered.strings')
 
 
 def run_socher_code():
@@ -57,7 +65,7 @@ def reformat_socher_vectors():
 
     """
     logging.info('Reformatting events file %s ---> %s',
-                 socher_output_vectors_file, vectors_file)
+                 socher_output_vectors_file, socher_composed_vectors_file)
 
     # socher's code removes all PoS tags, so we can't translate his output
     # back to a DocumentFeature. Let's read the input to his code instead and
@@ -81,9 +89,69 @@ def reformat_socher_vectors():
     write_vectors_to_hdf(sp.coo_matrix(mat),
                          composed_phrases,
                          ['RAE-feat%d' % i for i in range(100)],  # Socher provides 100-dimensional vectors
-                         vectors_file)
-    return vectors_file
+                         socher_composed_vectors_file)
 
+
+def write_clean_turian_unigrams():
+    """
+    Extracts unigram embeddings from Socher's binary distribution. These can be used by other composers.
+
+    There are only 50k embeddings (presumably for the most frequent tokens in the corpus). The words have not
+    been processed- there are punctuation-only tokens, uppercased words and non-lemmatized words. There isn't
+    any PoS tag filtering either- words like "to", "while" and "there".
+
+    I remove punctuation, then lowercase and lemmatize each entry. Multiple entries may map to the
+    same canonical form. I select the shortest original entry (ties are broken by giving preference to
+    words that are already lowercased). This could have been done better.
+    Only vectors for the selected entries are kept. There's 33k canonical
+    forms left, many of which are not nouns/adjs/verbs.
+
+    We don't have a PoS tag for the canonical forms. I get around the problem by creating 3 copies of each
+    canonical form and expand "cat" to cat/N, cat/J and cat/V, which all share the same vector.
+    """
+    logging.info('Writing Turian unigrams to %s', turian_unigram_vectors_file)
+    mat = loadmat(socher_unigram_embedding_matlab)
+    words = [w[0] for w in mat['words'].ravel()]
+    df = pd.DataFrame(mat['We'].T, index=words)
+
+    lmtzr = WordNetLemmatizer()
+    clean_to_dirty = defaultdict(list)  # canonical -> [non-canonical]
+    dirty_to_clean = dict()  # non-canonical -> canonical
+    to_keep = set()  # which non-canonical forms forms we will keep
+    #  todo this can be done based on frequency or something
+
+    for w in words:
+        if set(w).intersection(set(string.punctuation).union(set('0123456789'))):
+            # not a real word- contains digits or punctuation
+            continue
+
+        lemma = lmtzr.lemmatize(w.lower())
+        clean_to_dirty[lemma].append(w)
+        dirty_to_clean[w] = lemma
+
+    # decide which of possibly many non-canonical forms with the same lemma to keep
+    # prefer shorter and lowercased non-canonical forms
+    for lemma, dirty_list in clean_to_dirty.items():
+        if len(dirty_list) > 1:
+            best_lemma = min(dirty_list, key=lambda w: (len(w), not w.islower()))
+        else:
+            best_lemma = dirty_list[0]
+        to_keep.add(best_lemma)
+
+    # remove non-canonical forms we don't want
+    idx_to_drop = [i for i, w in enumerate(df.index) if w not in to_keep]
+    ddf = df.drop(df.index[idx_to_drop])
+    # canonicalize whatever is left
+    ddf.index = [lmtzr.lemmatize(w.lower()) for w in ddf.index]
+
+    # we don't know what the PoS tags of the canonical forms are, so make them all of the same tag
+    # e.g. expand "cat" to cat/N, cat/J and cat/V, which all share the same vector
+    new_index = ['%s/%s'%(w, pos) for pos in 'NJV' for w in ddf.index]
+    new_data = np.vstack([ddf.values] * 3)
+    ddf = pd.DataFrame(new_data, index= new_index)
+    dv = DenseVectors(ddf, allow_lexical_overlap=True)
+    dv.to_tsv(turian_unigram_vectors_file)
+    logging.info('Done')
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
@@ -102,11 +170,13 @@ if __name__ == '__main__':
     if 'fancy-compose' in args.stages:
         run_socher_code()
     if 'format' in args.stages:
+        # write just the unigram vectors for other composers to use
+        write_clean_turian_unigrams()
         reformat_socher_vectors()
     if 'simple-compose' in args.stages:
         composers = [AdditiveComposer, MultiplicativeComposer, LeftmostWordComposer,
                      RightmostWordComposer, VerbComposer]
-        compose_and_write_vectors(vectors_file,
+        compose_and_write_vectors(turian_unigram_vectors_file,
                                   'turian',
                                   composers,
                                   output_dir=output_dir, gzipped=False, dense_hd5=True)
