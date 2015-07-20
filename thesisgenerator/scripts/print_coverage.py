91ef77c4e71a826d19955a75ece0f21fa091a8ab
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-07-20 21:27:08 +0100

    blah

---
+++ thesisgenerator/scripts/print_coverage.py
@@ -0,0 +1,24 @@
+from discoutils.thesaurus_loader import Vectors
+from collections import Counter
+from discoutils.tokens import DocumentFeature
+
+
+def info(path):
+    print('------------', path)
+    v = Vectors.from_tsv(path)
+    print(Counter(DocumentFeature.from_string(x).type for x in v.keys()))
+    print('total', len(v))
+
+for c in 'Add Left Baroni Guevara Observed'.split():
+    path = '/mnt/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/exp11-13-composed-ngrams-ppmi-svd/AN_NN_wiki-100_%s.events.filtered.strings' % c
+    info(path)
+
+    path = '/mnt/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/exp10-13-composed-ngrams-ppmi-svd/AN_NN_gigaw-100_%s.events.filtered.strings' % c
+    info(path)
+
+for c in 'Add Left'.split():
+    path = '/mnt/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/word2vec_vectors/composed/AN_NN_word2vec-gigaw_100percent-rep0_%s.events.filtered.strings' % c
+    info(path)
+
+    path = '/mnt/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/word2vec_vectors/composed/AN_NN_word2vec-wiki_100percent-rep0_%s.events.filtered.strings' % c
+    info(path)
