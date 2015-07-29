44a55fefd2c4b09f135c28c27dd7db3fe203a0c8
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-07-29 14:46:10 +0100

    [WIP] more detailed analysis of coverage of vectors sets

--- thesisgenerator/scripts/print_coverage.py
+++ thesisgenerator/scripts/print_coverage.py
@@ -1,24 +1,31 @@
+import sys
+from joblib import delayed
+from joblib import Parallel
+
+sys.path.append('.')
 from discoutils.thesaurus_loader import Vectors
 from collections import Counter
 from discoutils.tokens import DocumentFeature
+from thesisgenerator.utils import db
+import pandas as pd
 
 
-def info(path):
-    print('------------', path)
-    v = Vectors.from_tsv(path)
-    print(Counter(DocumentFeature.from_string(x).type for x in v.keys()))
-    print('total', len(v))
+def info(**kwargs):
+    stats = {}
+    stats.update({'param_%s_' % k: v for k, v in kwargs.items()})
+    try:
+        v = Vectors.from_tsv(kwargs['path'])
+        keys = [DocumentFeature.from_string(x) for x in v.keys()]
 
-for c in 'Add Left Baroni Guevara Observed'.split():
-    path = '/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/exp11-13-composed-ngrams-ppmi-svd/AN_NN_wiki-100_%s.events.filtered.strings' % c
-    info(path)
+        type_counts = Counter(x.type for x in keys)
+        stats.update({'%s_count' % k: v for k, v in type_counts.items()})
 
-    path = '/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/exp10-13-composed-ngrams-ppmi-svd/AN_NN_gigaw-100_%s.events.filtered.strings' % c
-    info(path)
+        pos_counts = Counter(x.tokens[0].pos for x in keys if x.type == '1-GRAM')
+        stats.update({'%s_count' % k: v for k, v in pos_counts.items()})
+    finally:
+        return stats
 
-for c in 'Add Left'.split():
-    path = '/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/word2vec_vectors/composed/AN_NN_word2vec-gigaw_100percent-rep0_%s.events.filtered.strings' % c
-    info(path)
 
-    path = '/lustre/scratch/inf/mmb28/FeatureExtractionToolkit/word2vec_vectors/composed/AN_NN_word2vec-wiki_100percent-rep0_%s.events.filtered.strings' % c
-    info(path)
+if __name__ == '__main__':
+    res = Parallel(n_jobs=2)(delayed(info)(**v._data) for v in db.Vectors.select())  # todo run for all vectors
+    pd.DataFrame(res).to_csv('coverage_stats.csv')
